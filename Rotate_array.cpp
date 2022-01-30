cb9e982048535abb76443ee9ce36409528088a14
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-01-30 23:18:15 +0530

    Feat(Medium) Rotated array

--- Rotate_array.cpp
+++ Rotate_array.cpp
@@ -133,6 +133,29 @@ public:
     }
 };
 
+/// Approach-4
+///Runtime: 53 ms, faster than 23.40% of C++ online submissions for Rotate Array.
+//Memory Usage: 26.2 MB, less than 7.88% of C++ online submissions for Rotate Array.
+class Solution
+{
+public:
+    void rotate(vector<int> &nums, int k)
+    {
+
+        int n = nums.size();
+        k = k % nums.size();
+        for (int i = n - k; i < n; i++)
+        {
+            nums.push_back(nums[i]);
+        }
+        for (int i = 0; i < n - k; i++)
+        {
+            nums.push_back(nums[i]);
+        }
+        nums.erase(nums.begin(), nums.begin() + n);
+    }
+};
+
 int main()
 {
 
