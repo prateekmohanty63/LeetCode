5c712cdcca1b212b2f00a0d80e8cb096ee15ec50
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-02-08 09:20:39 +0530

    Feat(Number theory) Digital number concept

--- Number_theory/Add_digits.cpp
+++ Number_theory/Add_digits.cpp
@@ -36,6 +36,19 @@ public:
     }
 };
 
+/// 2nd approach
+/// Faster than 100%
+
+class Solution
+{
+public:
+    int addDigits(int num)
+    {
+
+        return 1 + (num - 1) % 9;
+    }
+};
+
 int main()
 {
 
