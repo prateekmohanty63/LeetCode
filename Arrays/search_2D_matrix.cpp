4aa87fd39c5e2f86185d6f4203a08c30880b1e2a
Prateek <prateekmohanty63@gmail.com>
2022-02-23 12:05:48 +0530

    Deat(array) Search 2d matrix

--- Arrays/search_2D_matrix.cpp
+++ Arrays/search_2D_matrix.cpp
@@ -2,6 +2,86 @@
 #include <vector>
 using namespace std;
 
+
+/// Correct solution
+// Runtime: 4 ms, faster than 78.24% of C++ online submissions for Search a 2D Matrix.
+// Memory Usage: 9.5 MB, less than 78.77% of C++ online submissions for Search a 2D Matrix.
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int n=matrix.size();
+        int m=matrix[0].size();
+        
+        int row=-1;
+        
+        for(int i=0;i<n;i++){
+            if(matrix[i][0]<target && matrix[i][m-1]>target){
+                row=i;
+            }
+            
+            if(matrix[i][0]==target || matrix[i][m-1]==target)return true;
+        }
+        
+        if(row==-1)return false;
+        for(int i=0;i<m;i++){
+            if(matrix[row][i]==target)return true;
+        }
+        cout<<row;
+        return false;
+    }
+};
+
+/// OPTIMIZED SOLUTION 
+class Solution {
+    
+    int binarySearch(vector<int>vec1,int l,int r,int target){
+        int mid=l+(r-l)/2;
+        
+        if(l<=r){
+        if(vec1[mid]==target)return mid;
+        
+        if(target<vec1[mid]){
+            return binarySearch(vec1,l,mid-1,target);
+        }
+        else{
+            return binarySearch(vec1,mid+1,r,target);
+        }
+        }
+        return -1;
+    }
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int n=matrix.size();
+        int m=matrix[0].size();
+        
+        int row=-1;
+        
+        for(int i=0;i<n;i++){
+            if(matrix[i][0]<target && matrix[i][m-1]>target){
+                row=i;
+            }
+            
+            if(matrix[i][0]==target || matrix[i][m-1]==target)return true;
+        }
+        
+        if(row==-1)return false;
+        
+        vector<int>vec1=matrix[row];
+        
+        int r=matrix[row].size();
+        
+        int index=binarySearch(vec1,0,r-1,target);
+        
+        if(index!=-1)return true;
+        cout<<row;
+        return false;
+    }
+};
+
 /// 1st approach
 /// Giving TLE
 class Solution
