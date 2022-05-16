68fb2303483c7980c64baf2bb88702f70a2082d6
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-05-16 15:56:02 +0530

    Feat(tree) Added count of complete tree node

--- Trees/countCompleteTreeNode.cpp
+++ Trees/countCompleteTreeNode.cpp
@@ -12,6 +12,12 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+// Runtime: 35 ms, faster than 80.77% of C++ online submissions for Count Complete Tree Nodes.
+// Memory Usage: 30.8 MB, less than 64.50% of C++ online submissions for Count Complete Tree Nodes.
+
+// Time complexity of this code is O(n)
+// in question time complexity of less than o(n) has been asked
+
 class Solution
 {
 public:
