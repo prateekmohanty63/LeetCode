99a3a9fbe96a60584e6ab5a44625cf2cfad00534
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-05-17 11:53:38 +0530

    Feat(Tree) Added uni valued tree

--- Trees/univaluedTree.cpp
+++ Trees/univaluedTree.cpp
@@ -39,6 +39,52 @@ public:
     }
 };
 
+// 2nd method
+
+class Solution
+{
+public:
+    bool isUnivalTree(TreeNode *root)
+    {
+
+        if (root == NULL)
+            return true;
+
+        if (root->left)
+            if (root->val != root->left->val)
+                return false;
+
+        if (root->right)
+            if (root->val != root->right->val)
+                return false;
+
+        return isUnivalTree(root->left) && isUnivalTree(root->right);
+    }
+};
+
+// 2nd method
+
+class Solution
+{
+public:
+    bool isUnivalTree(TreeNode *root)
+    {
+
+        if (root == NULL)
+            return true;
+
+        if (root->left)
+            if (root->val != root->left->val)
+                return false;
+
+        if (root->right)
+            if (root->val != root->right->val)
+                return false;
+
+        return isUnivalTree(root->left) && isUnivalTree(root->right);
+    }
+};
+
 int main()
 {
 
