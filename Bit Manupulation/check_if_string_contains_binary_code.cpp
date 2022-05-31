795dfb43633b940061c0fd88bb65123a27dfbf96
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-05-31 16:04:59 +0530

    Feat(Bit manupulation) Added check if binary string exists

--- Bit Manupulation/check_if_string_contains_binary_code.cpp
+++ Bit Manupulation/check_if_string_contains_binary_code.cpp
@@ -6,6 +6,8 @@ using namespace std;
 // Runtime: 1783 ms, faster than 5.03% of C++ online submissions for Check If a String Contains All Binary Codes of Size K.
 // Memory Usage: 91.4 MB, less than 16.98% of C++ online submissions for Check If a String Contains All Binary Codes of Size K.
 
+// 1st method
+
 class Solution
 {
 public:
@@ -39,6 +41,30 @@ public:
         if (set1.size() == m)
             return true;
 
+        return false;
+    }
+};
+
+// 2nd method
+
+//Runtime: 634 ms, faster than 32.39% of C++ online submissions for Check If a String Contains All Binary Codes of Size K.
+//Memory Usage: 71.7 MB, less than 42.77% of C++ online submissions for Check If a String Contains All Binary Codes of Size K.
+
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        
+        int n=s.size();
+        
+        unordered_set<string>set1;
+        
+        for(int i=0;i<=n-k;i++)
+        {
+            set1.insert(s.substr(i,k));
+        }
+        
+        if(set1.size()== (1<<k))return true;
+        
         return false;
     }
 };
