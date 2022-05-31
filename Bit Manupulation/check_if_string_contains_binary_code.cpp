07d23dcb9264786b7dc7e4dca2161a80da15d19b
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-05-31 15:58:20 +0530

    Feat(Bit manupulation) Added check if binary string exists

--- Bit Manupulation/check_if_string_contains_binary_code.cpp
+++ Bit Manupulation/check_if_string_contains_binary_code.cpp
@@ -1,44 +1,50 @@
-#include<iostream>
-#include<set>
-#include<math.h>
+#include <iostream>
+#include <set>
+#include <math.h>
 using namespace std;
 
-class Solution {
+// Runtime: 1783 ms, faster than 5.03% of C++ online submissions for Check If a String Contains All Binary Codes of Size K.
+// Memory Usage: 91.4 MB, less than 16.98% of C++ online submissions for Check If a String Contains All Binary Codes of Size K.
+
+class Solution
+{
 public:
-    bool hasAllCodes(string s, int k) {
-        
-        int n=s.length();
-        
-        set<string>set1;
-        int m=pow(2,k);
-        
-        for(int i=0;i<=n-k;i++)
+    bool hasAllCodes(string s, int k)
+    {
+
+        int n = s.length();
+
+        set<string> set1;
+        int m = pow(2, k);
+
+        for (int i = 0; i <= n - k; i++)
         {
             string a;
-            for(int j=i;j<k+i;j++)
+            for (int j = i; j < k + i; j++)
             {
-                a+=s[j];
-                
-               // break;
+                a += s[j];
+
+                // break;
             }
-    
+
             set1.insert(a);
         }
-        
+
         // set elements
-         set<string>::iterator itr;
-        
-        for(itr=set1.begin();itr!=set1.end();itr++)
-            cout<<*itr<<" ";
-        
-        if(set1.size()==m)return true;
-        
+        set<string>::iterator itr;
+
+        for (itr = set1.begin(); itr != set1.end(); itr++)
+            cout << *itr << " ";
+
+        if (set1.size() == m)
+            return true;
+
         return false;
     }
 };
 
 int main()
 {
-    
+
     return 0;
 }
