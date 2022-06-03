be29f393ce2009504ae169c3f1303f29efbab607
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-06-03 13:55:45 +0530

    Feat(Recursion) FInd winner in circular game

---
+++ Recursion/FindWinner_circular_game.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+// 1st method
+class Solution {
+public:
+    void findWinner(int &n,int k,int &count,int &res)
+    {
+        if(n==1){
+            res=count;
+            return ;
+        }
+        
+        int temp=k;
+        
+        while(temp)
+        {
+            count++;
+            if(count>n)
+                count=1;
+            temp--;
+            
+        }
+        n--;
+        findWinner(n,k,count,res);
+    }
+    int findTheWinner(int n, int k) {
+        int count=0;
+        int res;
+        
+        findWinner(n,k,count,res);
+        
+       if(res==n)return 1;
+        return res+1;
+    }
+};
+
+// 2nd method
+// time complexity : O(n)
+// space complexity : O(n)
+//Runtime: 4 ms, faster than 71.21% of C++ online submissions for Find the Winner of the Circular Game.
+//Memory Usage: 5.9 MB, less than 91.71% of C++ online submissions for Find the Winner of the Circular Game.
+
+class Solution {
+public:
+    int winner(int n,int k)
+    {
+        if(n==1)return 0;
+        
+        return (winner(n-1,k)+k)%n;
+    }
+    int findTheWinner(int n, int k) {
+        
+        int a=winner(n,k)+1;
+        
+        return a;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
