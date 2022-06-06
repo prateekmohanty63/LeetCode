2fe27cae34deaf5970732d418d10ef1176d58eb1
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-06-06 15:39:09 +0530

    Feat(Greedy) Added partition into deci binary numbers

---
+++ Greedy/Partition_into_deci_binary_numbers.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int minPartitions(string n) {
+        
+        int m=n.length();
+        int count=0;
+        
+        // for all single digit numbers
+        if(m==1)
+        {
+            return stoi(n); 
+        }
+        
+        int max=INT_MIN;
+        
+        for(int i=0;i<m;i++)
+        {
+            int x = (int)n[i] - 48;
+            
+            if(x>max)max=x;
+        }
+        return max;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
