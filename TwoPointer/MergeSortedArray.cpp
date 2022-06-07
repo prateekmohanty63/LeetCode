d551f4eff8b40734068fe1b266ffd89f8f59406e
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-06-07 11:57:31 +0530

    Feat(Two pointer) Added Merge Sorted Array

--- TwoPointer/MergeSortedArray.cpp
+++ TwoPointer/MergeSortedArray.cpp
@@ -3,6 +3,45 @@
 #include<algorithm>
 using namespace std;
 
+// Runtime: 7 ms, faster than 24.43% of C++ online submissions for Merge Sorted Array.
+// Memory Usage: 9.2 MB, less than 30.68% of C++ online submissions for Merge Sorted Array.
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        vector<int>auxiliary;
+      
+        
+        int i = 0, j = 0, k = 0;
+ 
+    // Traverse both array
+    while (i<m && j<n)
+    {
+        if (nums1[i] < nums2[j])
+            auxiliary.push_back(nums1[i++]);
+        else
+            auxiliary.push_back(nums2[j++]);
+    }
+        
+        while(i!=m)
+        {
+            auxiliary.push_back(nums1[i++]);
+        
+        }
+         while(j!=n)
+        {
+            auxiliary.push_back(nums2[j++]);
+           
+        }
+        
+        
+        for(int i=0;i<auxiliary.size();i++)
+           nums1[i]=auxiliary[i];
+    }
+};
+
 // Runtime: 4 ms, faster than 49.07% of C++ online submissions for Merge Sorted Array.
 // Memory Usage: 9.1 MB, less than 30.68% of C++ online submissions for Merge Sorted Array.
 // runtime is O(nlog)
