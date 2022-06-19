cac466fc73581ac2f19c22979d887db84af4f095
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-06-19 15:05:51 +0530

    Added Trie structure

--- Trie_Questions/Sarch_suggestion_questions.cpp
+++ Trie_Questions/Sarch_suggestion_questions.cpp
@@ -4,6 +4,63 @@
 using namespace std;
 
 
+
+// Optimized version of brute force mechanism
+// Runtime: 631 ms, faster than 17.07% of C++ online submissions for Search Suggestions System.
+// Memory Usage: 194.4 MB, less than 12.94% of C++ online submissions for Search Suggestions System
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        
+        vector<vector<string>>res;
+        
+        sort(products.begin(),products.end());
+        
+        int n=products.size();
+        int m=searchWord.size();
+        
+        for(int i=0;i<n;i++)
+            cout<<products[i]<<" ";
+        
+        string wordPre;
+        int k=1;
+        int begin=0;
+        
+        for(int i=0;i<m;i++)
+        {
+             int l=0;
+            wordPre+=searchWord[i];
+            vector<string>vec1;
+            for(int j=begin;j<n;j++)
+            {
+                if(l==3)break;
+                
+                string str=products[j];
+                string str1=str.substr(0,k);
+                if(str1==wordPre)
+                {
+                    if(l==0)
+                    {
+                        begin=j;
+                    }
+                    vec1.push_back(str);
+                    l++;
+                }
+                
+                
+            }
+            
+            k++;
+            res.push_back(vec1);
+        }
+        
+        
+        
+        return res;
+    }
+};
+
+
 // this is brute force method
 // Runtime: 1397 ms, faster than 7.13% of C++ online submissions for Search Suggestions System.
 // Memory Usage: 332 MB, less than 11.41% of C++ online submissions for Search Suggestions System.
