1d326b416a35f71ba2cc60ba2ef91972e172efed
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-06-22 10:49:42 +0530

    Feat(array) Added kth largest element in the array

--- Arrays/Kth_largest_elemnt_in_array.cpp
+++ Arrays/Kth_largest_elemnt_in_array.cpp
@@ -1,7 +1,34 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<queue>
 using namespace std;
+
+
+// using priority queue
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        
+        int n=nums.size();
+        
+        priority_queue<int>que;
+        
+        for(int i=0;i<n;i++)
+        {
+            que.push(nums[i]);
+        }
+        
+        int res;
+        
+        while(k!=1)
+        {
+            que.pop();
+            k--;
+        }
+        return que.top();
+    }
+};
 
 
 
