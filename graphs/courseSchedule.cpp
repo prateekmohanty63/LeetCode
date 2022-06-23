61395a10a69ccaff20806b02aaac2e2818620100
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-06-23 21:08:13 +0530

    Added graphs

---
+++ graphs/courseSchedule.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    
+    bool isCyclic(vector<vector<int>>&adj,vector<int>&visited,int curr)
+    {
+        if(visited[curr]==2)return true;
+        
+        visited[curr]=2;
+        
+        for(int i=0;i<adj[curr].size();i++)
+        {
+            if(visited[adj[curr][i]!=1])
+                if(isCyclic(adj,visited,adj[curr][i]))return true;
+        }
+        visited[curr]=1;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        int n=prerequisites.size();
+        vector<vector<int>>adj(numCourses);
+        
+        //  making an adjancey list
+        for(int i=0;i<n;i++)
+        {
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        
+        vector<int>visited(numCourses,0);
+        
+        for(int i=0;i<numCourses;i++)
+        {
+            if(visited[i]==0)
+                if(isCyclic(adj,visited,i))return false;
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
