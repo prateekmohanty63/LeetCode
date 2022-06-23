b521a7a50688d163362d043164072b91f6ee103f
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-06-23 22:15:31 +0530

    Feat(priority queue) added courses schedule 2

--- PriorityQueue/course_schedule2.cpp
+++ PriorityQueue/course_schedule2.cpp
@@ -3,8 +3,8 @@
 #include<queue>
 using namespace std;
 
-Runtime: 423 ms, faster than 33.28% of C++ online submissions for Course Schedule III.
-Memory Usage: 63.6 MB, less than 21.80% of C++ online submissions for Course Schedule III.
+// Runtime: 423 ms, faster than 33.28% of C++ online submissions for Course Schedule III.
+// Memory Usage: 63.6 MB, less than 21.80% of C++ online submissions for Course Schedule III.
 
 class Solution {
 public:
