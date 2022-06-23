48bdb38ca33d3b0450527190e07de3c6ee7e1afd
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-06-23 21:29:38 +0530

    Feat(graph) added course sehedule

--- graphs/courseSchedule.cpp
+++ graphs/courseSchedule.cpp
@@ -3,6 +3,40 @@
 using namespace std;
 
 
+// Runtime: 47 ms, faster than 19.78% of C++ online submissions for Course Schedule.
+// Memory Usage: 14.3 MB, less than 38.60% of C++ online submissions for Course Schedule.
+
+class Solution {
+public:
+    
+   bool iscycle(vector<int> adj[],vector<int> &vis,int id){
+        if(vis[id]==1)
+            return true;
+        if(vis[id]==0){
+            vis[id]=1;
+            for(auto edge : adj[id]){
+                if(iscycle(adj,vis,edge))
+                    return true;
+            }
+        }
+        vis[id] = 2;
+        return false;
+    }
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<int> adj[n];
+        for(auto edge : pre)
+            adj[edge[1]].push_back(edge[0]);
+        vector<int> vis(n,0);
+        
+        for(int i=0;i<n;i++){
+            if(iscycle(adj,vis,i))
+                return false;
+        }
+        return true;
+    }
+};
+
+
 // gives TLE
 class Solution {
 public:
