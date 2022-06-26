95249c3461e9e87765383f74bfd484ef6974f276
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-06-26 10:10:06 +0530

    Feat(Sliding window) added maximum points obtained from cards

---
+++ slidingWindow/maximumPoints_obtained_cards.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// code correct
+// though giving memory limit exceeded
+class Solution {
+public:
+    
+    int sum(vector<int>vec1,int start,int end)
+    {
+        int sum1=0;
+        for(int i=start;i<=end;i++)
+        {
+            sum1+=vec1[i];
+        }
+        return sum1;
+    }
+    int maxScore(vector<int>& cardPoints, int k) {
+        
+        int n=cardPoints.size();
+        int res=0;
+        
+       
+        for(int i=0;i<n;i++)res+=cardPoints[i];
+        
+        if(n==k)return res;
+        
+        
+        int right=n-k-1,left=0;
+        
+        int min=INT_MAX;
+        
+        while(right!=n)
+        {
+            int sum2=sum(cardPoints,left,right);
+            if(sum2<min)min=sum2;
+            
+            left++;
+            right++;
+        }
+        
+        return res-min;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
