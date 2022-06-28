9b306212d358c294fb1e984e9ed5748c69e891e1
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-06-28 12:53:57 +0530

    Feat(Array) added image rotation

--- Arrays/ImageRotation.cpp
+++ Arrays/ImageRotation.cpp
@@ -1,6 +1,28 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
+
+
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Rotate Image.
+// Memory Usage: 7 MB, less than 92.54% of C++ online submissions for Rotate Image.
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+         int n = matrix.size();
+        
+        //first we take transpose of the matrix
+        for(int i = 0; i < n; ++i){
+            for(int j = i; j < n; ++j){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for(int i = 0; i < n; ++i){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
 
 // Runtime: 12 ms, faster than 9.01% of C++ online submissions for Rotate Image.
 // Memory Usage: 7.3 MB, less than 33.06% of C++ online submissions for Rotate Image.
