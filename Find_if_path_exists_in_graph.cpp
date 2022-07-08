91b1e1ba0c53e97ab76aed4c2d53d55391ccb081
Prateek Mohanty <72243320+prateekmohanty63@users.noreply.github.com>
2022-07-08 13:58:42 +0530

    Graph question
    Feat(graph) Added find if path exists

--- Find_if_path_exists_in_graph.cpp
+++ Find_if_path_exists_in_graph.cpp
@@ -4,7 +4,51 @@
 #include<map>
 #include<vector>
 #include<unordered_map>
+#include<queue>
 
+
+// using BFS
+// Runtime: 981 ms, faster than 53.36% of C++ online submissions for Find if Path Exists in Graph.
+// Memory Usage: 207.4 MB, less than 33.15% of C++ online submissions for Find if Path Exists in Graph.
+
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        
+        // using bfs
+        
+        if(source==destination)return true;
+        
+        unordered_map<int,list<int>>adj;
+        
+        for(auto edge:edges)
+        {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+        
+        queue<int>q;
+        vector<int>visited(n);
+        q.push(source);
+        visited[source]=true;
+        
+        while(!q.empty())
+        {
+            int front=q.front();
+            q.pop();
+            for(auto neighbour:adj[front])
+            {
+                if(!visited[neighbour]){
+                if(neighbour==destination)return true;
+                visited[neighbour]=true;
+                
+                q.push(neighbour);
+                }
+            }
+        }
+        return false;
+    }
+};
 
 
 
