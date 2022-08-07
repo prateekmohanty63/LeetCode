841252869b26545de1c719088e72291a713914cc
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-08-07 23:48:58 +0530

    Feat(DP) Added count vowels permutation

--- Dynamic Programming/countVowelsPermutation.cpp
+++ Dynamic Programming/countVowelsPermutation.cpp
@@ -1,7 +1,103 @@
 #include <iostream>
-#include<vector>
-#include<unordered_map>
+#include <vector>
+#include <unordered_map>
+#include<algorithm>
 using namespace std;
+
+class Solution {
+public:
+    int countVowelPermutation(int n) {
+        vector<vector<long long int> > dp(n, vector<long long int>(5,0));
+        int mod = 1e9+7;
+        
+        for(int i=0;i<5;i++){
+            dp[0][i]=1;
+        }
+        
+        for(int i=1;i<n;i++){
+            dp[i][0] = (dp[i-1][1] + dp[i-1][2] + dp[i-1][4])%mod;
+            
+            dp[i][1] = (dp[i-1][2] + dp[i-1][0])%mod;
+            
+            dp[i][2] = (dp[i-1][1] + dp[i-1][3])%mod;
+            
+            dp[i][3] = (dp[i-1][2])%mod;
+            
+            dp[i][4] = (dp[i-1][3] + dp[i-1][2])%mod;
+        }
+        
+        long long int sum=0;
+        
+        for(int i=0;i<5;i++){
+            sum+=dp[n-1][i];
+            sum%=mod;
+        }
+        
+        return sum;
+    }
+};
+
+
+
+// method correct giving error for bigger inputs
+
+
+class Solution
+{
+public:
+    int MOD = 100000007;
+    unordered_map<string, long> dp;
+    long helper(int remNo, char prevChar)
+    {
+        // base case
+        if (remNo == 0)
+            return 1;
+
+        long total = 0;
+
+        string res = to_string(remNo) + "*" + prevChar;
+
+        if (dp.find(res) != dp.end())
+            return dp[res];
+
+        switch (prevChar)
+        {
+        case 'a':
+            total = helper(remNo - 1, 'e') % MOD;
+            break;
+
+        case 'e':
+            total = (helper(remNo - 1, 'a') + helper(remNo - 1, 'i')) % MOD;
+            break;
+
+        case 'i':
+            total = (helper(remNo - 1, 'a') + helper(remNo - 1, 'e') + helper(remNo - 1, 'o') + helper(remNo - 1, 'u')) % MOD;
+            break;
+
+        case 'o':
+            total = (helper(remNo - 1, 'i') + helper(remNo - 1, 'u')) % MOD;
+            break;
+
+        case 'u':
+            total = (helper(remNo - 1, 'a')) % MOD;
+            break;
+        }
+        return dp[res] = total;
+    }
+    int countVowelPermutation(int n)
+    {
+
+        vector<char> vowel = {'a', 'e', 'i', 'o', 'u'};
+
+        long totalCount = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            totalCount = (totalCount + helper(n - 1, vowel[i])) % MOD;
+        }
+        return (int)totalCount;
+    }
+};
 
 class Solution
 {
