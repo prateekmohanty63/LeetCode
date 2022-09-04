7430e42cadaf27f99e12d5e19385a7bc36b1aa1c
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-09-04 06:20:32 +0530

    Feat(DP) added number of ways to reach a position in exactly k steps

--- Dynamic Programming/numberofWaysToReachAPositionAfterKSteps.cpp
+++ Dynamic Programming/numberofWaysToReachAPositionAfterKSteps.cpp
@@ -1,7 +1,48 @@
 #include<iostream>
 #include<vector>
 #include<unordered_map>
+#include<set>
 using namespace std;
+
+
+// Runtime: 253 ms, faster than 50.00% of C++ online submissions for Number of Ways to Reach a Position After Exactly k Steps.
+// Memory Usage: 33.3 MB, less than 50.00% of C++ online submissions for Number of Ways to Reach a Position After Exactly k Steps.
+
+class Solution {
+public:
+
+int mod = 1e9 + 7;
+long dp[3100][1001];
+
+long helper(int start , int end , int k){
+    if(start==end){
+       if(k==0) return 1;
+    }
+    if(k==0) return 0;
+
+    
+    // plus 1000 is done so that the index can stay non-negative
+    // because max we can go 1000 steps back (negative)
+    if(dp[start+1000][k]!=-1) return dp[start+1000][k];
+
+
+    long ans = helper(start+1,end,k-1);
+
+  
+    if((k-1) >= (end-start+1)){
+        ans += helper(start-1,end,k-1);
+    }
+    return dp[start+1000][k]=ans%mod;
+}
+int numberOfWays(int sp, int ep, int k) {
+    memset(dp,-1,sizeof(dp));
+    if(abs(sp-ep)>k) return 0;
+    
+    // helper
+    long ans = helper(sp,ep,k);
+    return ans%mod;
+}
+};
 
 
 // 27/35 test-cases
