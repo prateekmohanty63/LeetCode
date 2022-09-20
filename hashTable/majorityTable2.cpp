080d9ef9821d289ddfa23ab0a2a76245d26a434d
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-09-20 16:17:37 +0530

    Feat(Hashtable) added majority table 2

--- hashTable/majorityTable2.cpp
+++ hashTable/majorityTable2.cpp
@@ -4,6 +4,36 @@
 #include<set>
 using namespace std;
 
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        
+        int n=nums.size();
+        
+        vector<int>res;
+        
+        unordered_map<int,int>mp;
+   
+        for(auto it:nums)
+        {
+            mp[it]++;
+        }
+        
+        for(auto it:mp)
+        {
+            if(it.second>floor(n/3) && it.second!=-1)
+            {
+                res.push_back(it.first);
+                it.second=-1;
+                
+
+            }
+        }
+        return res;
+    }
+};
+
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
