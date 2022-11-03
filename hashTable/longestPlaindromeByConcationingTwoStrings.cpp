99e8d419e74e405200b02075947ff6a26cbb1595
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-11-03 09:34:46 +0530

    Feat(hashtable) added Longest Plaindrome by concatining two strings

---
+++ hashTable/longestPlaindromeByConcationingTwoStrings.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        
+        map<string,int>mp;
+        int res=0;
+        
+        for(int i=0;i<words.size();i++)
+        {
+            mp[words[i]]++;
+        }
+        
+        bool flag=false;
+        
+        for(auto x:words)
+        {
+            string w=x;
+            reverse(w.begin(),w.end());
+            
+            if(w!=x && mp[w]>0 && mp[x]>0)
+            {
+                res+=4;
+                mp[w]--;
+                mp[x]--;
+                
+            }
+            else if(w==x && mp[x]>1)
+            {
+                mp[x]-=2;
+                res+=4;
+            }
+            else if(w==x && mp[x]>0 && !flag)
+            {
+                mp[x]--;
+                res+=2;
+                flag=1;
+            }
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
