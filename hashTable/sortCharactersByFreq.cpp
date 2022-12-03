15fa795a46ae70f5d3c42f3e3fd603228881e58b
Prateek <prateekmohanty63@gmail.com>
2022-12-03 11:20:37 +0530

    Feat(hashtable) added sort chracters by frequency

---
+++ hashTable/sortCharactersByFreq.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+
+
+class Solution {
+public:
+
+  static bool cmp(pair<char, int>& a,
+         pair<char, int>& b)
+{
+    return a.second > b.second;
+}
+ 
+    string frequencySort(string s) {
+             
+             unordered_map<char,int>mp;
+
+             int n=s.length();
+
+
+             for(int i=0;i<n;i++)
+             {
+                 mp[s[i]]++;
+             }
+
+             vector<pair<char,int>>vec1;
+
+             for(auto it:mp)
+             {
+                 vec1.push_back(it);
+             }
+        
+        sort(vec1.begin(),vec1.end(),cmp);
+
+        string res;
+
+        for(auto it:vec1)
+        {
+            for(int i=0;i<it.second;i++)
+            {
+                res+=it.first;
+            }
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
