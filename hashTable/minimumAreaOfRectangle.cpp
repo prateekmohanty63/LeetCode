c4e0c937c5f64ee64fcf1bf28415b50f77cdd476
Prateek <prateekmohanty63@gmail.com>
2022-12-09 21:23:36 +0530

    Feat(Hashtable) Added minimum area of rectangle

---
+++ hashTable/minimumAreaOfRectangle.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<unordered_map>
+#include<map>
+using namespace std;
+
+class Solution {
+public:
+    
+    int reactArea(int x1,int x2,int x3,int x4,int y1,int y2,int y3,int y4)
+    {
+        int side1=sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+        int side2=sqrt((x1-x3)*(x1-x3)+(y1-y3)*(y1-y3));
+
+        return side1*side2;
+    }
+   
+
+    int minAreaRect(vector<vector<int>>& points) {
+
+         int n=points.size();
+        
+        sort(points.begin(),points.end());
+       map<pair<int,int>,bool>mp;
+
+       for(int i=0;i<n;i++)
+       {
+           mp[make_pair(points[i][0],points[i][1])]=true;
+       }
+
+       int maxArea=INT_MAX;
+
+       
+
+        for(int i=0;i<n;i++)
+        {
+              int x1=points[i][0];
+              int y1=points[i][1];
+
+              for(int j=i+1;j<n;j++)
+              {
+                  int x2=points[j][0];
+                  int y2=points[j][1];
+
+                  if(x2>x1)break;
+
+                
+
+                  for(int k=j+1;k<n;k++)
+                  {
+                      int x3=points[k][0];
+                      int y3=points[k][1];
+
+                  if(y1!=y3)continue;
+
+                      if(x1==x2 && y1==y3 && mp.find(make_pair(x3,y2))!=mp.end())
+                      {
+                           int area=reactArea(x1,x2,x3,x3,y1,y2,y3,y2);
+
+                           maxArea=min(maxArea,area);
+                      }
+                  }
+              }
+
+ 
+        }
+
+        if(maxArea==INT_MAX)return 0;
+
+        return maxArea;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
