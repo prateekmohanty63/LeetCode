f12d57494ab1d7b1c0f9aa66383b566369403a11
Prateek <prateekmohanty63@gmail.com>
2022-12-09 21:46:05 +0530

    Feat(Hashtable) Added minimum area of rectangle

--- hashTable/minimumAreaOfRectangle.cpp
+++ hashTable/minimumAreaOfRectangle.cpp
@@ -6,6 +6,64 @@
 using namespace std;
 
 
+// Find rectangle considering points in the for loop are diagonal points
+class Solution {
+public:
+    
+    int reactArea(int x1,int x2,int x3,int x4,int y1,int y2,int y3,int y4)
+    {
+        int side1=sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+        int side2=sqrt((x1-x3)*(x1-x3)+(y1-y3)*(y1-y3));
+
+        return side1*side2;
+    }
+   
+
+    int minAreaRect(vector<vector<int>>& points) {
+
+         int n=points.size();
+        
+       // sort(points.begin(),points.end());
+       unordered_map<int,vector<int>>mp;
+
+       for(int i=0;i<n;i++)
+       {
+           mp[points[i][0]].push_back(points[i][1]);
+       }
+
+       int maxArea=INT_MAX;
+
+       
+     // use diangonals to find the rectangle
+
+     for(int i=0;i<n;i++)
+     {
+         int x1=points[i][0];
+         int y1=points[i][1];
+
+         for(int j=i+1;j<n;j++)
+         {
+            int x2=points[j][0];
+            int y2=points[j][1];
+
+            if(x1==x2 || y1==y2)continue;
+
+            if(find(mp[x1].begin(),mp[x1].end(),y2)!=mp[x1].end() && find(mp[x2].begin(),mp[x2].end(),y1)!=mp[x2].end())
+            {
+                maxArea=min(maxArea,abs(x2-x1)*abs(y2-y1));
+            }
+
+
+         }
+     }
+       
+        if(maxArea==INT_MAX)return 0;
+
+        return maxArea;
+    }
+};
+
+
 // TLE
 //114/137 passing
 
