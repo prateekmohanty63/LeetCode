677535f70e0ddf1a0bfff2bd1fbc53ccf894d63d
Prateek <prateekmohanty63@gmail.com>
2022-12-15 15:44:01 +0530

    Feat(string) Added maximize subsequences in a string

--- strings/maximizeSubsubsequencesInASrting.cpp
+++ strings/maximizeSubsubsequencesInASrting.cpp
@@ -3,6 +3,62 @@
 using namespace std;
 
 // TLE
+// 163
+
+class Solution {
+public:
+    long long maxOccurance(string text,string pattern)
+    {
+        char firstChar=pattern[0];
+        char secondChar=pattern[1];
+        int n=text.length();
+
+        long long count=0;
+
+        for(int i=0;i<text.length();i++)
+        {
+            if(text[i]!=firstChar)continue;
+
+            for(int j=i+1;j<n;j++)
+            {
+                if(text[j]==secondChar)count++;
+            }
+        }
+        return count;
+    }
+
+    long long maximumSubsequenceCount(string text, string pattern) {
+        
+        long long res=0;
+         int n=text.length();
+         long long maxRes=INT_MIN;
+
+         char a=pattern[0];
+         string a1;
+         a1+=a;
+
+
+         char b=pattern[1];
+         string b1;
+         b1+=b;
+
+        // cout<<a1<<" "<<b1;
+        
+        string temp=text;
+        temp.insert(0,a1);
+        maxRes=max(maxRes,maxOccurance(temp,pattern));
+
+        temp=text;
+        temp.insert(n,b1);
+        cout<<temp;
+        maxRes=max(maxRes,maxOccurance(temp,pattern));
+
+
+        return maxRes;
+    }
+};
+
+// TLE
 // 127/172
 
 class Solution {
