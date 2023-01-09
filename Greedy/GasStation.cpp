5f007959f84584d8c0a375891e46b5298e9a7875
Prateek <prateekmohanty63@gmail.com>
2023-01-09 13:21:04 +0530

    Feat(greedy) Added has station

--- Greedy/GasStation.cpp
+++ Greedy/GasStation.cpp
@@ -2,48 +2,64 @@
 #include<vector>
 using namespace std;
 
+
+// Corrrect solution 
+// but TLE
+
 class Solution {
 public:
 
 
    
-    int cirularIndex(vector<int>&gas,vector<int>&cost,int i,int target,int currGas)
+    int circularIndex(vector<int>&gas,vector<int>&cost,int i,int target,int currGas)
     {
         // base case
         int n=gas.size();
 
-         if(currGas>=cost[i%n] && (i+1)%n==target)return target;
+        if((i+1)%n==target && currGas>=cost[i%n])return target;
 
-         if(currGas<=cost[i%n])return -1;
+        if(currGas<=cost[i%n])return -1;
 
-        if(currGas<=0)return -1;
+        
+        int gasLeft=currGas-cost[i%n];
 
-        int gasLeft=currGas-cost[i]+gas[(i+1)%n];
-
-       return cirularIndex(gas,cost,(i+1)%n,target,gasLeft);
+        return circularIndex(gas,cost,(i+1)%n,target,gasLeft+gas[(i+1)%n]);
+        
+      
 
 
     }
  
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int n=gas.size();
+        int res=-1;
+
+        int totalCost=0;
+        int totalGas=0;
+
+        // for(int i=0;i<n;i++)
+        // {
+        //     totalCost+=cost[i];
+        //     totalGas+=gas[i];
+        // }
         
+        // if(totalCost<totalGas)return -1;
+
         //cout<<cirularIndex(gas,cost,3,4,4);
      
         for(int k=0;k<n;k++)
         {
             int target=k;
             int currGas=gas[k];
-            int start=(k+1)%n;
+            int start=k;
 
-            int res=cirularIndex(gas,cost,start,target,currGas);
+            res=circularIndex(gas,cost,k,k,currGas);
 
             if(res!=-1)return res;
         }
-        return -1;
+        return res;
     }
 };
-
 int main()
 {
     
