38d7f7bdb542d393dc966168a27c582b5e82acff
Prateek <prateekmohanty63@gmail.com>
2023-02-01 22:20:59 +0530

    Feat(COMPANY-WISE : MICROSOFT) Added cinemaSeatAllocation

--- COMPANY-WISE/Microsoft/cinemaSeatAllocation.cpp
+++ COMPANY-WISE/Microsoft/cinemaSeatAllocation.cpp
@@ -4,6 +4,77 @@
 using namespace std;
 
 
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        
+        unordered_map<int,vector<int>>mp;
+        int m=reservedSeats.size();
+
+        for(int i=0;i<m;i++)
+        {
+            mp[reservedSeats[i][0]].push_back(reservedSeats[i][1]);
+        }
+
+        int res=(n-mp.size())*2;
+
+        for(auto it:mp)
+        {
+            int m=it.second.size();
+
+            if(m==1)
+            {
+               int rev=it.second[0];
+
+               if(rev==1 || rev==10)res+=2;
+
+               else res++;   
+            }
+            else if(m==2)
+            {
+                int rev1=it.second[0];
+                int rev2=it.second[1];
+
+                if((rev1==1 && rev2==10) || (rev1==10 && rev2==1))res+=2;
+                
+                else{
+                     vector<int>vec=it.second;
+                bool first=true,second=true,third=true;
+
+                for(int i=0;i<m;i++)
+                {
+                    if(vec[i]==2 || vec[i]==3 || vec[i]==4 || vec[i]==5)first=false;
+                    if(vec[i]==6 || vec[i]==7 || vec[i]==8 || vec[i]==9)second=false;
+                    if(vec[i]==4 || vec[i]==5 || vec[i]==6 || vec[i]==7)third=false;
+
+                } 
+                
+                if(first || second || third)res++;
+                }
+                
+            }
+            else{
+                vector<int>vec=it.second;
+                bool first=true,second=true,third=true;
+
+                for(int i=0;i<m;i++)
+                {
+                    if(vec[i]==2 || vec[i]==3 || vec[i]==4 || vec[i]==5)first=false;
+                    if(vec[i]==6 || vec[i]==7 || vec[i]==8 || vec[i]==9)second=false;
+                    if(vec[i]==4 || vec[i]==5 || vec[i]==6 || vec[i]==7)third=false;
+
+                } 
+                
+                if(first || second || third)res++;
+               
+               
+            }
+        }
+        return res;
+    }
+};
+
+
 // GIVING TLE
 class Solution {
 public:
