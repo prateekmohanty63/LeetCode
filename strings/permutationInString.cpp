274cfcd178eca49545bc81bba0b7a355f54a1d4d
Prateek <prateekmohanty63@gmail.com>
2023-02-04 12:39:34 +0530

    Feat(String) Added permutations in string

--- strings/permutationInString.cpp
+++ strings/permutationInString.cpp
@@ -3,7 +3,66 @@
 using namespace std;
 
 
-// passed all test cases
+// optimized solution
+// Time complexity: O(N)
+// Space complexity : O(26)
+
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        
+        int n=s1.length();
+        int m=s2.length();
+
+        if(s1==s2)return true;
+
+        if(m<n)return false;
+
+       // if(n==m && s1!=s2)return false;
+
+        vector<int>chars(26,-1);
+        vector<int>chars1(26,-1);
+
+        for(int i=0;i<n;i++)
+        {
+            chars[s1[i]-'a']+=1;
+        }
+        
+        int ptr1=0;
+        int ptr2=n;
+
+        for(int i=0;i<ptr2;i++)
+        {
+            chars1[s2[i]-'a']+=1;
+        }
+
+       if(chars==chars1)return true;
+
+        while(ptr2<m)
+        {
+             
+
+            char left=s2[ptr1];
+            char right=s2[ptr2];
+            cout<<left<<" "<<right<<endl;
+
+            chars1[left-'a']--;
+            chars1[right-'a']++;
+
+            ptr2++;
+            ptr1++;
+
+          if(chars==chars1)return true;
+            
+        }
+
+       return false;
+    }
+};
+
+
+// passed all test cases  
+// Time complexity: O(N*N*logN)
 
 class Solution {
 public:
