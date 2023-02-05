e92b7e539b8b8cd741e71a62cefc4e77b218d57b
Prateek <prateekmohanty63@gmail.com>
2023-02-05 10:50:34 +0530

    Feat(String) Added find all anagrams in a string

--- strings/findAllAnagramsInAString.cpp
+++ strings/findAllAnagramsInAString.cpp
@@ -3,6 +3,75 @@
 #include<algorithm>
 using namespace std;
 
+
+
+// All testcases passed
+// two pointer approach
+// Time Complexity: O(n*26) == O(n)
+// space complexity: O(26)
+
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        
+        // sorting p 
+
+       // sort(p.begin(),p.end());
+
+        vector<int>chars(26,-1);
+        vector<int>chars1(26,-1); 
+        vector<int>res;
+
+       
+
+         int m=p.length();
+         int n=s.length();
+
+          int start=0;
+          int end=m-1;
+
+         if(n<m)return res;
+
+         for(int i=0;i<m;i++)
+         {
+             chars[p[i]-'a']++;
+         }
+         
+         for(int i=0;i<m;i++)
+         {
+             chars1[s[i]-'a']++;
+         }
+        
+           if(chars==chars1)
+            {
+                res.push_back(start);
+            }
+
+       
+
+        while(end+1<s.length())
+        {
+           
+            
+            chars1[s[start]-'a']--;
+            chars1[s[end+1]-'a']++;
+ 
+             start++;
+            end++;
+
+            if(chars==chars1)
+            {
+                res.push_back(start);
+            }
+
+
+            
+        }
+
+        return res;
+    }
+};
+
 // Logic correct (TLE)
 // 34/64 passed
 
