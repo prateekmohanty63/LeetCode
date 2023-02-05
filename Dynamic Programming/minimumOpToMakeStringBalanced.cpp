6b97794a523eee480663cc3293affe9f1d94b138
Prateek <prateekmohanty63@gmail.com>
2023-02-05 13:39:43 +0530

    Feat(Dynamic programming) Added minim op to make string balanced

--- Dynamic Programming/minimumOpToMakeStringBalanced.cpp
+++ Dynamic Programming/minimumOpToMakeStringBalanced.cpp
@@ -1,5 +1,59 @@
 #include<iostream>
+#include<unordered_map>
 using namespace std;
+
+
+// 79/157 cases passed (TLE)
+class Solution {
+public:
+   
+   int minOp(string s,int i,char change,unordered_map<string,int>&mp)
+   {
+       // base case
+       if(i>=s.length())return 0;
+
+       string a=to_string(i);
+       a+=change;
+
+       if(mp.find(a)!=mp.end())return mp[a];
+
+       if(s[i]=='a' && change=='a')
+       {
+           return mp[a]=minOp(s,i+1,'a',mp);
+       }
+
+       else if(s[i]=='a' && change=='b')
+       {
+           return mp[a]=1+minOp(s,i+1,'b',mp);
+       }
+
+       else if(s[i]=='b' && change=='a')
+       {
+           return mp[a]=min(1+minOp(s,i+1,'a',mp),minOp(s,i+1,'b',mp));
+       }
+       else 
+       {
+           return mp[a]=minOp(s,i+1,'b',mp);
+       }
+   }
+
+    int minimumDeletions(string s) {
+        
+        int ptr=0;
+
+        while(ptr<s.length() && s[ptr]=='a')ptr++;
+
+       // string is already balanced
+
+        if(ptr==s.length())return 0;
+
+        unordered_map<string,int>mp;
+
+        int res=minOp(s,0,'a',mp);
+
+        return res;
+    }
+};
 
 // 37/157 cases passed (TLE)
 class Solution {
