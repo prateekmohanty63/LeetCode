d46d22f1f89a5a22e22b9e04eadd555d5b3df757
Prateek <prateekmohanty63@gmail.com>
2023-02-06 11:18:45 +0530

    Feat(Array) Shuffle the array

--- Arrays/shuffleTheArray.cpp
+++ Arrays/shuffleTheArray.cpp
@@ -8,6 +8,30 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         
+        
+        
+        
+        int ptr2=n;
+        int k=1;
+
+        for(int i=0;i<n;i++)
+        { 
+            nums.insert(nums.begin()+k,nums[ptr2]);
+            k+=2;
+            ptr2+=2;
+        }
+
+        for(int i=0;i<n;i++)nums.pop_back();
+
+        return nums;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        
         vector<int>res;
 
         int ptr1=0;
