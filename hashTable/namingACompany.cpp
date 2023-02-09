9478415979cca785b90668cddc6998cf89f6fe49
Prateek <prateekmohanty63@gmail.com>
2023-02-09 14:51:54 +0530

    Feat(HashTable) Added naming a company

--- hashTable/namingACompany.cpp
+++ hashTable/namingACompany.cpp
@@ -1,7 +1,37 @@
 #include<iostream>
 #include<unordered_map>
 #include<vector>
+#include<unordered_set>
 using namespace std;
+
+
+class Solution {
+public:
+   
+ long distinctNames(vector<string> ideas) {
+     
+        unordered_set<string> groups[26];
+        for (auto idea : ideas) {
+            groups[idea[0] - 'a'].insert(idea.substr(1));
+        }
+
+        long answer = 0;
+        for (int i = 0; i < 25; ++i) {
+            for (int j = i + 1; j < 26; ++j) {
+                
+                long mutuals = 0;
+                for (auto ideaA : groups[i]) {
+                    if (groups[j].count(ideaA)) {
+                        mutuals++;
+                    }
+                }
+                answer += 2 * (groups[i].size() - mutuals) * (groups[j].size() - mutuals);
+            }
+        }
+        
+        return answer;
+ }
+};
 
 
 class Solution {
