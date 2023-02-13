32dad3f873e7fbf1e7f8c1b4ba3aa2adc01f9f06
Prateek <prateekmohanty63@gmail.com>
2023-02-13 12:23:44 +0530

    Feat(Math) Added count odd number in a interval range

--- Math/countOddNumberInAIntervalRange.cpp
+++ Math/countOddNumberInAIntervalRange.cpp
@@ -8,6 +8,19 @@ public:
         
         int res=0; 
 
+        res=(high+1)/2-(low)/2;
+
+        return res;
+    }
+};
+
+
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        
+        int res=0; 
+
         for(int i=low;i<=high;i++)
         {
             if(i%2!=0)res++;
