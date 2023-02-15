f77415db471fb30979fd578dc704f586cabf18b6
Prateek <prateekmohanty63@gmail.com>
2023-02-15 11:22:20 +0530

    Feat(Math || Array) Added add arry form of integer

---
+++ Math/AddToArrayFormOfInteger.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include <cmath>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        
+        long long res=0;
+        vector<int>vec;
+
+        int n=num.size();
+        int l=1;
+
+        for(int i=0;i<n;i++)
+        {
+            res+=num[i]*pow(10,n-l);
+            l++;
+        }
+
+        res+=k;
+
+        string sum=to_string(res);
+
+        for(int i=0;i<sum.length();i++)
+        {
+            char a=sum[i];
+            int r=int(a)-48;
+            vec.push_back(r);
+        }
+
+        cout<<res;
+
+        return vec;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
