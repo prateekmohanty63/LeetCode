80101391698dc0ec53d2414493b300f965eb08f5
Prateek <prateekmohanty63@gmail.com>
2023-02-15 21:12:26 +0530

    Feat(Math) Added add array to integer

--- Math/AddToArrayFormOfInteger.cpp
+++ Math/AddToArrayFormOfInteger.cpp
@@ -3,6 +3,40 @@
 #include <cmath>
 using namespace std;
 
+
+
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        int carry=0;
+        int j=num.size()-1;
+
+        while(j>=0)
+        {
+            int sum=num[j]+carry+k%10;
+            k/=10;
+
+            num[j--]=sum%10;
+            carry=sum/10;
+        }
+
+        while(k>0)
+        {
+            int sum=(k%10)+carry;
+            k/=10;
+            num.insert(num.begin(),sum%10);
+            carry=sum/10;
+        }
+
+        if(carry>0)
+        {
+            num.insert(num.begin(),carry);
+        }
+
+      return num;
+    }
+};
+
 // Giving precision error
 // going out of long long , because array is of size 10^4
 
