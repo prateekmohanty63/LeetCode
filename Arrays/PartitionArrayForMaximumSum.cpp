7eb64ac1ae6957c9813f833ecf1b7eb79aaf840c
Prateek <prateekmohanty63@gmail.com>
2023-02-21 14:45:31 +0530

    Feat:Array Added parition array for maximum sum

--- Arrays/PartitionArrayForMaximumSum.cpp
+++ Arrays/PartitionArrayForMaximumSum.cpp
@@ -4,6 +4,47 @@
 using namespace std;
 
 
+// Passed all test-cases 
+
+class Solution {
+public:
+   
+   int maxSumPossible(vector<int>&arr,int i,int k,vector<int>&dp)
+   {
+       // base case
+
+       if(i>=arr.size())return 0;
+
+       int n=arr.size();
+
+       int maxim=INT_MIN;
+
+       int maxRes=INT_MIN;
+
+       if(dp[i]!=-1)return dp[i];
+
+        for(int j=i;j<min(n,i+k);j++)
+        {
+             maxim=max(maxim,arr[j]);
+
+             int sum=(j-i+1)*maxim;
+
+             maxRes=max(maxRes,sum+maxSumPossible(arr,j+1,k,dp));
+        }
+        return dp[i]=maxRes;
+   }
+
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+
+        int n=arr.size();
+        vector<int>dp(n+1,-1);
+        int res=maxSumPossible(arr,0,k,dp);
+
+        return res;
+    }
+};
+
+
 // solutiong giveing TLE (need to convert to dp)
 class Solution {
 public:
