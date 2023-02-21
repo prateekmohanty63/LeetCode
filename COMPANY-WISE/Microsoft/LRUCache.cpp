2a8b4b7961f67a6ac6d9ca01dbb475a28b4d60a5
Prateek <prateekmohanty63@gmail.com>
2023-02-21 15:48:06 +0530

    Feat:Microsoft Added LRU Cache

---
+++ COMPANY-WISE/Microsoft/LRUCache.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+
+
+class LRUCache {
+public:   
+
+    int maxCapacity;
+    unordered_map<int,int>cache;
+    vector<int>usedOrder;
+
+
+    LRUCache(int capacity) {
+         
+         maxCapacity=capacity;
+    }
+    
+    int get(int key) {
+
+        if(cache.find(key)!=cache.end()){
+        
+          for(int i=0;i<usedOrder.size();i++)
+          {
+              if(usedOrder[i]==key)
+              {
+                  usedOrder.erase(usedOrder.begin()+i);
+                  break;
+              }
+          }
+
+          usedOrder.push_back(key);
+          return cache[key];
+        }
+
+        return -1;
+        
+    }
+    
+    void put(int key, int value) {
+        
+        if(cache.find(key)!=cache.end())
+        {
+            for(int i=0;i<usedOrder.size();i++)
+            {
+                if(usedOrder[i]==key)
+                {
+                    usedOrder.erase(usedOrder.begin()+i);
+                    break;
+                }
+            }
+            usedOrder.push_back(key);
+
+            cache[key]=value;
+            return ;
+        }
+
+        maxCapacity--;
+
+        if(maxCapacity<0)
+        {
+               
+             int keyToRemove=usedOrder[0];
+
+            cache.erase(keyToRemove);
+            usedOrder.erase(usedOrder.begin());
+        }
+        cache[key]=value;
+        usedOrder.push_back(key);
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+
+int main()
+{
+    
+    return 0;
+}
