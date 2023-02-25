be623080b22f9c682eec2a469e834fe0263f8a0c
Prateek <prateekmohanty63@gmail.com>
2023-02-25 14:09:36 +0530

    Feat:DP Added maximum score with number of points

--- Dynamic Programming/maximumNumberOfPointsWithCost.cpp
+++ Dynamic Programming/maximumNumberOfPointsWithCost.cpp
@@ -3,7 +3,44 @@
 using namespace std;
 
 
-// logic is correct , giving TLE as T(N)=2^n
+// giving TLE after 140 cases
+
+class Solution {
+public:
+   
+   long long maxPoint(vector<vector<int>>&points,int i,int j,int prev,int n,int m,vector<vector<vector<int>>>&dp)
+   {
+       // base case
+      
+      if(i>=n)return 0;
+
+      if(dp[i][j][prev+1]!=-1)return dp[i][j][prev+1];
+
+      if(j>=m)return maxPoint(points,i+1,0,prev,n,m,dp);
+      
+      if(prev!=-1 && i!=0)
+      {
+          return dp[i][j][prev+1]=max((points[i][j]-(abs(j-prev)))+maxPoint(points,i+1,0,j,n,m,dp),maxPoint(points,i,j+1,prev,n,m,dp));
+      }
+      else{
+          return dp[i][j][prev+1]=max(points[i][j]+maxPoint(points,i+1,0,j,n,m,dp),maxPoint(points,i,j+1,prev,n,m,dp));
+      }
+   }
+
+    long long maxPoints(vector<vector<int>>& points) {
+        
+         int n=points.size();
+        int m=points[0].size();
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(m+1,vector<int>(m+1,-1)));
+       
+        long long res=maxPoint(points,0,0,-1,n,m,dp);
+
+        return res;
+    }
+};
+
+
+// logic is correct , giving TLE as T(N)=2^n , 50 test cases
 class Solution {
 public:
    
