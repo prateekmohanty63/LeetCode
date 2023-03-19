c3fc1152a4a5825ed70c91757d4b39c311ce333c
Prateek <prateekmohanty63@gmail.com>
2023-03-19 12:05:47 +0530

    Feat:Microsoft Added minimum insetion to balance a paranthesis

---
+++ COMPANY-WISE/Microsoft/minimumInsertionsToBalanceAParanthesisString.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+
+class Solution {
+public:
+    int minInsertions(string s) {
+        
+        s+='*';
+        int n=s.length();
+        int count=0;
+        stack<char>stk;
+
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(')
+            {
+                stk.push('(');
+            }
+            else if(s[i]==')')
+            {
+                if(s[i+1]==')')
+                {
+                    if(!stk.empty())
+                    {
+                        stk.pop();
+                        i++;
+                    }
+                    else if(stk.empty())
+                    {
+                        count++;
+                        i++;
+                    }
+                }
+                else
+                {
+                    if(!stk.empty())
+                    {
+                        count++;
+                        stk.pop();
+                    }
+                    else if(stk.empty())
+                    {
+                        count+=2;
+                    }
+                }
+                
+            }
+        }
+
+        while(!stk.empty())
+        {
+            count+=2;
+            stk.pop();
+        }
+        return count;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
