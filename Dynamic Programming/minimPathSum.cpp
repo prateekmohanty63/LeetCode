ea9fafce7a39f4b0a9ff9f09ba3211620cd03830
Prateek <prateekmohanty63@gmail.com>
2023-03-27 15:40:22 +0530

    Feat:DP Added minimu path sum

--- Dynamic Programming/minimPathSum.cpp
+++ Dynamic Programming/minimPathSum.cpp
@@ -2,6 +2,43 @@
 #include<vector>
 using namespace std;
 
+// tabulation method
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        
+        int n=grid.size();
+        int m=grid[0].size();
+
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==0 && j==0)
+                {
+                    dp[i][j]=grid[i][j];
+                }
+                else{
+                    int a=10e7;
+                    int b=10e7;
+
+                    if(i>0)
+                    a=dp[i-1][j];
+
+                    if(j>0)b=dp[i][j-1];
+
+                    dp[i][j]=grid[i][j]+min(a,b);
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
+
+// memoization solution 
 
 class Solution {
 public:
