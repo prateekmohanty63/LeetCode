5d1f92f65a7d4571b6f057b97065b5f7efdfb1a5
Prateek <prateekmohanty63@gmail.com>
2023-03-30 19:15:29 +0530

    Feat:LinkedList Added rotateList

--- LinkList/rotateList.cpp
+++ LinkList/rotateList.cpp
@@ -12,6 +12,72 @@ using namespace std;
       ListNode(int x, ListNode *next) : val(x), next(next) {}
   };
  
+
+class Solution {
+public:
+
+   ListNode* rotateR(ListNode *head,int k)
+   {
+       
+       ListNode *temp=head;
+       vector<int>vec,vec1;
+
+       while(temp!=NULL)
+       {
+           vec.push_back(temp->val);
+           temp=temp->next;
+           
+       }
+
+       int n=vec.size();
+       
+       for(int i=n-k;i<n;i++)
+       {
+           vec1.push_back(vec[i]);
+       }
+
+       for(int i=0;i<n-k;i++)
+       {
+           vec1.push_back(vec[i]);
+       }
+     
+
+       ListNode *temp1=head;
+
+       int l=0;
+
+       while(temp1!=NULL)
+       {
+           temp1->val=vec1[l++];
+           temp1=temp1->next;
+       }
+
+       return head;
+   } 
+  
+    ListNode* rotateRight(ListNode* head, int k) {
+
+        if(head==NULL)return head;
+        int len=0;
+
+        ListNode *temp=head;
+
+        while(temp!=NULL)
+        {
+            len++;
+            temp=temp->next;
+        }
+        
+          k=k%len;
+      
+        ListNode *res=rotateR(head,k);
+
+        return res;
+    }
+}; 
+
+
+
 class Solution {
 public:
 
