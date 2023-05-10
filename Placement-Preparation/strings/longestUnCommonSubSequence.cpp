6296857db284a8593f3989569b9b1dc763fcd1f1
Prateek <prateekmohanty63@gmail.com>
2023-05-10 11:15:18 +0530

    Feat:String added longest uncommon subsequence

--- Placement-Preparation/strings/longestUnCommonSubSequence.cpp
+++ Placement-Preparation/strings/longestUnCommonSubSequence.cpp
@@ -3,6 +3,55 @@
 #include<climits>
 using namespace std;
 
+
+// Faster than 100%
+class Solution {
+public:
+   
+    bool isSubSequence(string a,string b,int i,int j)
+    {
+        // base case
+        if(i==0)return true;
+
+        if(j==0)return false;
+
+
+        if(a[i-1]==b[j-1])return isSubSequence(a,b,i-1,j-1);
+
+        return isSubSequence(a,b,i,j-1);
+    }
+
+    int findLUSlength(vector<string>& strs) {
+        
+        int maximLen=INT_MIN;
+        int n=strs.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            
+            string a=strs[i];
+            int len=a.length();
+            if(len<=maximLen)continue;
+            int flag=0;
+            for(int j=0;j<n;j++)
+            {
+                if(i!=j)
+                {
+                     if(isSubSequence(a,strs[j],a.length(),strs[j].length()))
+                     {
+                         flag=1;
+                         break;
+                     }
+                }
+            }
+            if(flag==0)maximLen=max(maximLen,len);
+        }
+        
+        if(maximLen==INT_MIN)return -1;
+        return maximLen;
+    }
+};
+
 // Time complexity: O(n^3)
 
 class Solution {
