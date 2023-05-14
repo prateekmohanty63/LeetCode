e6a42ecec2451d31796844cdca261d411c624d3b
Prateek <prateekmohanty63@gmail.com>
2023-05-14 12:13:05 +0530

    Feat:Added bit-manipulation

--- Placement-Preparation/BitManipulation/maximOR.cpp
+++ Placement-Preparation/BitManipulation/maximOR.cpp
@@ -2,6 +2,51 @@
 #include<vector>
 using namespace std;
 
+
+
+class Solution {
+public:
+   
+   long long maxOr(vector<int>&vec)
+   {
+       long long res=0;
+       int n=vec.size();
+       for(int i=0;i<n;i++)
+        {
+            res|=vec[i];
+        }
+        return res;
+   }
+
+    long long maximumOr(vector<int>& nums, int k) {
+
+    int n=nums.size();
+    long long res=-1;
+
+    vector<int>prefixOr(n+1),suffixOr(n+1);
+
+    for(int i=1;i<n;i++)
+    {
+        prefixOr[i]=prefixOr[i-1] | nums[i-1];
+        suffixOr[n-i-1]=suffixOr[n-i] | nums[n-i];
+    }    
+     
+    long long maxRes=-1;
+
+    for(int i=0;i<n;i++)
+    {
+      
+        maxRes=max(maxRes,(prefixOr[i] | ((long long)nums[i]<<k)|(suffixOr[i])));
+      
+   }   
+
+     return maxRes;
+  }
+};
+
+
+// T(n)=O(N^2)
+
 class Solution {
 public:
    
