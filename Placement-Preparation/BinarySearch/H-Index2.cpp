23ae8940e2fc77c98b2f0608b81a4a3aaaf4124e
Prateek <prateekmohanty63@gmail.com>
2023-05-15 11:59:17 +0530

    Feat:Binary-search added H-Index2

--- Placement-Preparation/BinarySearch/H-Index2.cpp
+++ Placement-Preparation/BinarySearch/H-Index2.cpp
@@ -3,6 +3,37 @@
 #include<climits>
 using namespace std;
 
+
+// T(n)=O(logn)
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        
+        int start=0;
+        int end=citations.size()-1;
+        int maxRes=-1;
+        int n=citations.size();
+
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+
+            if(citations[mid]>=n-mid)
+            {
+                maxRes=max(maxRes,n-mid);
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        if(maxRes==-1)return 0;
+        return maxRes;
+    }
+};
+
+
+// T(N)=O(nlogn)
 class Solution {
 public:
    
