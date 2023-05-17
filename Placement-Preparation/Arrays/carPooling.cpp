9c168e7f63d68e79ee41285ff9e5e5252588f6b3
Prateek <prateekmohanty63@gmail.com>
2023-05-17 12:25:26 +0530

    Feat:Array added car-pooling

---
+++ Placement-Preparation/Arrays/carPooling.cpp
@@ -0,0 +1,58 @@
+#include<vector>
+#include<algorithm>
+
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        
+       vector<vector<int>>vec;
+
+       int n=trips.size();
+
+
+       for(int i=0;i<n;i++)
+       {
+           vector<int>vec1;
+           vec1.push_back(trips[i][1]);
+           vec1.push_back(trips[i][2]);
+           vec1.push_back(trips[i][0]);
+           vec.push_back(vec1);
+       }
+
+       // sorting the vec based on to-from and passangers
+
+       sort(vec.begin(),vec.end());
+
+       if(capacity<vec[0][2])return false;
+
+       int m=vec.size();
+
+       int startPoint=vec[0][0];
+       int endPoint=vec[m-1][1];
+       int capRem=capacity;
+       int indexComp=0;
+
+       for(int i=startPoint;i<=endPoint;i++)
+       {
+           for(int j=indexComp;j<m;j++)
+           {
+               if(i==vec[j][0])
+               {
+                   int cap=vec[j][2];
+                   if(capRem<cap)return false;
+
+                   capRem-=cap;
+                   
+               }
+               if(i==vec[j][1])
+               {
+                   capRem+=vec[j][2];
+               }
+               
+           }
+           
+       }
+
+        return true;
+    }
+};
