384074e59168dec4100dd55160be81979345e99c
Prateek <prateekmohanty63@gmail.com>
2023-05-18 13:38:25 +0530

    Feat:DP Added min-cost-climbing-staris

--- Placement-Preparation/DP/minCostClimbingStairs.cpp
+++ Placement-Preparation/DP/minCostClimbingStairs.cpp
@@ -2,6 +2,22 @@
 #include<vector>
 using namespace std;
 
+// T=O(N)
+// S=O(N)
+ int minCostBottomUp(vector<int>&cost)
+   {
+       int n=cost.size();
+       vector<int>dp(n+2);
+       dp[n]=dp[n+1]=0;
+
+       for(int i=n-1;i>=0;i--)
+       {
+           dp[i]=cost[i]+min(dp[i+1],dp[i+2]);
+       }
+       return min(dp[0],dp[1]);
+   }
+
+
 class Solution {
 public:
    
