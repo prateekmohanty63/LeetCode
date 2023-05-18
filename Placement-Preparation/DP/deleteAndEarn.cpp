c517faeb8ff20a18cb278b55795ce9ca13dad233
Prateek <prateekmohanty63@gmail.com>
2023-05-18 12:09:28 +0530

    Feat:DP added deleteAndEarn

--- Placement-Preparation/DP/deleteAndEarn.cpp
+++ Placement-Preparation/DP/deleteAndEarn.cpp
@@ -7,6 +7,58 @@ using namespace std;
 class Solution {
 public:
     
+    int maxPoints(vector<int>&nums,int i,unordered_map<int,int>&mp,unordered_map<int,int>&freqMap,vector<int>&dp)
+    {
+        // base case
+        if(i<0)return 0;
+
+        // we have the option of deleting the element or not deleting it 
+        int val=nums[i];
+
+        if(dp[i]!=-1)return dp[i];
+       
+
+        if(mp.find(val-1)!=mp.end())
+        {
+            int indToGo=mp[val-1];
+            return dp[i]=max(val*freqMap[val]+maxPoints(nums,indToGo-1,mp,freqMap,dp),maxPoints(nums,i-1,mp,freqMap,dp));
+        }
+        else{
+            return dp[i]=max(val*freqMap[val]+maxPoints(nums,mp[val]-1,mp,freqMap,dp),maxPoints(nums,i-1,mp,freqMap,dp));
+        }
+        
+
+    }
+
+    int deleteAndEarn(vector<int>& nums) {
+        
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        unordered_map<int,int>indMap;
+        unordered_map<int,int>freqMap;
+        vector<int>dp(n+1,-1);
+
+        for(int i=0;i<n;i++)
+        {
+            if(indMap.find(nums[i])!=indMap.end())
+            indMap[nums[i]]=min(i,indMap[nums[i]]);
+
+            else indMap[nums[i]]=i;
+
+            freqMap[nums[i]]++;
+        }
+        for(auto it:indMap)cout<<it.first<<" "<<it.second<<endl;
+        cout<<endl;
+
+        for(auto it:freqMap)cout<<it.first<<" "<<it.second<<endl;
+
+        return maxPoints(nums,n-1,indMap,freqMap,dp);
+    }
+};
+
+class Solution {
+public:
+    
     int maxPoints(vector<int>&nums,int i,unordered_map<int,int>&mp,unordered_map<int,int>&freqMap)
     {
         // base case
