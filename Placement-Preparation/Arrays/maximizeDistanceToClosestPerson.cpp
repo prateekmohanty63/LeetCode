97db3eacfa71cc0b3c7983a6dce7e7b0664ffd1f
Prateek <prateekmohanty63@gmail.com>
2023-05-19 11:08:19 +0530

    Feat:Array added maximze-distance-to-closest-person

--- Placement-Preparation/Arrays/maximizeDistanceToClosestPerson.cpp
+++ Placement-Preparation/Arrays/maximizeDistanceToClosestPerson.cpp
@@ -3,6 +3,66 @@
 #include<climits>
 using namespace std;
 
+
+// T(n)=O(n)
+// s(n)=o(n)
+
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        
+        int n=seats.size();
+
+        vector<int>closestFromLeft(n,0);
+        vector<int>closestFromRight(n,0);
+
+        // checking the closest distance from left 
+        if(seats[0]==1)closestFromLeft[0]=0;
+        else closestFromLeft[0]=n;
+
+        for(int i=1;i<n;i++)
+        {
+            if(seats[i]==1)closestFromLeft[i]=i;
+
+            else closestFromLeft[i]=closestFromLeft[i-1];
+        }
+
+        // checking the closest from right
+
+        if(seats[n-1]==1)closestFromRight[n-1]=n-1;
+        else closestFromRight[n-1]=-1;
+
+        for(int i=n-2;i>=0;i--)
+        {
+            if(seats[i]==1)closestFromRight[i]=i;
+
+            else closestFromRight[i]=closestFromRight[i+1];
+        }
+
+        vector<int>pos(n,0);
+
+        for(int i=0;i<n;i++)
+        { 
+            int a=abs(closestFromLeft[i]-i);
+            int b=abs(i-closestFromRight[i]);
+            pos[i]=min(a,b);
+        }
+
+        for(auto it:pos)cout<<it<<" ";
+
+        int maximDist=INT_MIN;
+
+        for(auto it:pos)
+        {
+            maximDist=max(maximDist,it);
+        } 
+        return maximDist;
+    }
+};
+
+
+//  T(n)=O(N^2)
+// S(N)=O(1)
 class Solution {
 public:
    int dist(vector<int>&seats,int pos)
