e50979bc554928ca482676c4616ca8b745caba67
Prateek <prateekmohanty63@gmail.com>
2023-05-25 13:02:46 +0530

    Feat:Graph added topological sort

--- Placement-Preparation/Greedy/Maximum Subsequence Score.cpp
+++ Placement-Preparation/Greedy/Maximum Subsequence Score.cpp
@@ -5,6 +5,61 @@
 #include<priority_queue>
 using namespace std;
 
+
+// T(N)=O()
+class Solution {
+public:
+
+ 
+static bool sortrev(const pair<int,int> &a,  const pair<int,int> &b) 
+{ 
+  return (a.first > b.first); 
+}
+    long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+        
+       vector<pair<int,int>>vec;
+       int n=nums1.size();
+       long long res=LONG_MIN;
+
+       for(int i=0;i<nums1.size();i++)
+       {
+           pair<int,int>pr;
+           pr.first=nums2[i];
+           pr.second=nums1[i];
+           vec.push_back(pr);
+       }
+
+       sort(vec.begin(),vec.end(),sortrev);
+       for(auto it:vec)
+       cout<<it.first<<" "<<it.second<<endl;
+
+       long long sum=0;
+       // min heap
+      priority_queue <int, vector<int>, greater<int> > pq;
+
+
+       for(int i=0;i<n;i++)
+       {
+         sum+=vec[i].second;
+         pq.push(vec[i].second);
+
+         if(pq.size()>k)
+         {
+            int numToRem=pq.top();
+            pq.pop();
+            sum-=numToRem;
+         }
+         if(pq.size()==k)
+         {
+             res=max(res,sum*vec[i].first);
+         }
+     }
+       
+      
+        return res;
+    }
+};
+
 class Solution {
 public:
 
