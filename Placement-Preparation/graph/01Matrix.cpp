77cfdb8d349915adbf296adf046f267448f3a17e
Prateek <prateekmohanty63@gmail.com>
2023-05-26 14:19:45 +0530

    Feat:Graph added 01Matrix

--- Placement-Preparation/graph/01Matrix.cpp
+++ Placement-Preparation/graph/01Matrix.cpp
@@ -4,6 +4,57 @@
 #include<algorithm>
 using namespace std;
 
+
+// TLE
+
+class Solution {
+public:
+
+   vector<vector<int>>dist={{-1,0},{1,0},{0,-1},{0,1}};
+   
+  
+  void dfs(vector<vector<int>>&mat,int i,int j,vector<vector<int>>&vis,int level)
+  {
+      // base case
+      int n=mat.size();
+      int m=mat[0].size();
+      if(i<0 || i>=n || j<0 || j>=m || (mat[i][j]==0 && level!=0))
+      return ;
+      
+      // there is a shorter path already present
+      if(vis[i][j] && mat[i][j]<=level)return ;
+
+      mat[i][j]=level;
+
+      vis[i][j]=1;
+
+      for(int k=0;k<4;k++)
+      {
+          dfs(mat,i+dist[k][0],j+dist[k][1],vis,level+1);
+      }
+  }
+
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        
+        int n=mat.size();
+        int m=mat[0].size();
+
+        vector<vector<int>>vis(n,vector<int>(m,0));
+
+        for(int i=0;i<n;i++)
+        { 
+            for(int j=0;j<m;j++)
+            {
+                if(mat[i][j]==0)
+                {
+                    dfs(mat,i,j,vis,0);
+                }
+            }
+        }
+        return mat;
+    }
+};
+
 class Solution {
 public:
   
