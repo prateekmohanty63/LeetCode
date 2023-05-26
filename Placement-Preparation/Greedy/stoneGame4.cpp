1bc8c9fdb186a92caf6bc7d448f090240c8d6296
Prateek <prateekmohanty63@gmail.com>
2023-05-26 12:17:43 +0530

    Feat:Greedy Added stone-game-4

--- Placement-Preparation/Greedy/stoneGame4.cpp
+++ Placement-Preparation/Greedy/stoneGame4.cpp
@@ -1,7 +1,62 @@
 #include<iostream>
 #include<vector>
 #include<climits>
+#include<queue>
 using namespace std;
+
+
+class Solution {
+public:
+    int stoneGameVI(vector<int>& aliceValues, vector<int>& bobValues) {
+        int n=aliceValues.size();
+        int temp=n;
+        int alicePoints=0;
+        int bobPoints=0;
+        int turn=0;
+
+       
+        vector<pair<int,vector<int>>>vec;
+        priority_queue<pair<int,vector<int>>>q;
+        for(int i=0;i<aliceValues.size();i++)
+        {
+            
+            pair<int,vector<int>>pr;
+            vector<int>vec1;
+            pr.first=aliceValues[i]+bobValues[i];
+            vec1.push_back(aliceValues[i]);
+            vec1.push_back(bobValues[i]);
+            pr.second=vec1;
+            q.push(pr);
+            
+        }
+       
+      
+       while(!q.empty())
+       {
+           pair<int,vector<int>>pr=q.top();
+           q.pop();
+
+           if(turn%2==0)
+           {
+               alicePoints+=pr.second[0];
+           }
+           else{
+               bobPoints+=pr.second[1];
+           }
+           turn++;
+       }
+        
+
+       
+        cout<<alicePoints<<" "<<bobPoints<<endl;
+
+        if(alicePoints==bobPoints)return 0;
+
+        if(alicePoints>bobPoints)return 1;
+
+        return -1;
+    }
+};
 
 // TLE 
 // T(N)=O(n^2)
