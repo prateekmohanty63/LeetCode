13b5fcc3b431dcf1fe30589a14f57b12e904c98c
Prateek <prateekmohanty63@gmail.com>
2023-06-03 17:20:09 +0530

    Feat:Graph added most stones removed from same row or column

--- Placement-Preparation/graph/mostStonesRemovedFromSameRowOrColumn.cpp
+++ Placement-Preparation/graph/mostStonesRemovedFromSameRowOrColumn.cpp
@@ -3,6 +3,36 @@
 #include<climits>
 using namespace std;
 
+// Passing all test cases 
+class Solution {
+public:
+    int n ;
+    void dfs(vector<vector<int>>& stones, int index, vector<bool>& visited) {
+        visited[index] = true;
+        
+        for(int i = 0; i<n; i++) {
+            if(!visited[i] &&
+               ((stones[i][0] == stones[index][0]) || (stones[i][1] == stones[index][1]))) {
+                dfs(stones, i, visited);
+            }
+        }
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        n = stones.size();
+        vector<bool> visited(n, false);
+        
+        int count = 0;
+        for(int i = 0; i<n; i++) {
+            if(visited[i])
+                continue;
+            dfs(stones, i, visited);
+            count++;
+        }
+        
+        return n - count;
+    }
+};
+
 // TLE
 
 class Solution {
