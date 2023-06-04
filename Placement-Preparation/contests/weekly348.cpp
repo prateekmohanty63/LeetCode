ff54e238523bb018db9fc6814458b9caa63a3304
Prateek <prateekmohanty63@gmail.com>
2023-06-04 11:25:36 +0530

    Feat:Weekly-348 added Sem-Ordered Permutation

--- Placement-Preparation/contests/weekly348.cpp
+++ Placement-Preparation/contests/weekly348.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 
-// minimum string left
+// 1.minimum string left
 class Solution {
 public:
     int minimizedStringLength(string s) {
@@ -25,6 +25,34 @@ public:
             }
         }
         return mp.size();
+    }
+};
+
+// 2. Semi-Ordered Permutation
+class Solution {
+public:
+    int semiOrderedPermutation(vector<int>& nums) {
+        
+        int n=nums.size();
+        
+        if(nums[0]==1 && nums[n-1]==n)return 0;
+        
+        int pos1=-1;
+        int posn=-1;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==1)pos1=i;
+            
+            if(nums[i]==n)posn=i;
+        }
+        
+        if(pos1<posn)return (pos1-0)+((n-1)-posn);
+        
+        else{
+            return (pos1-0)+((n-1)-posn)-1;
+        }
+        
     }
 };
 
