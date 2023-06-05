5ba120f3c9dae57e663a6b7a9434cc2f380ba6be
Prateek <prateekmohanty63@gmail.com>
2023-06-05 12:02:36 +0530

    Feat:Stack added largest area in histogram

--- Placement-Preparation/stack/largestAreaInHistogram.cpp
+++ Placement-Preparation/stack/largestAreaInHistogram.cpp
@@ -1,7 +1,91 @@
 #include<iostream>
 #include<vector>
+#include<stack>
 #include<climits>
 using namespace std;
+
+
+// Optimized 
+// T(N)=O()
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        
+        int n=heights.size();
+        int maxArea=INT_MIN;
+
+        vector<int>left;
+        vector<int>right(n,0);
+
+        // checking the smallest from left 
+        left.push_back(1);
+
+        // stack 
+        stack<int>stk;
+        stk.push(0);
+
+        for(int i=1;i<n;i++)               // <- O(N)
+        {
+            if(heights[stk.top()]<heights[i])
+            {
+                left.push_back(i-stk.top());
+            }
+            else{
+                while(!stk.empty() && heights[stk.top()]>=heights[i])stk.pop();   // O(1)
+
+                if(stk.empty())
+                {
+                    left.push_back(i+1);
+                }
+                else{
+                    left.push_back(i-stk.top());
+                }
+            }
+            stk.push(i);
+        }
+
+        stack<int>stk1;
+        int k=n-1;
+        right[k--]=1;
+
+        stk1.push(n-1);
+
+        for(int i=n-2;i>=0;i--)
+        {
+            if(heights[stk1.top()]<heights[i])
+            {
+                 right[k--]=stk1.top()-i;
+            }
+            else{
+                while(!stk1.empty() && heights[stk1.top()]>=heights[i])
+                stk1.pop();
+
+                if(stk1.empty())
+                {
+                    //cout<<i<<" ";
+                    right[k--]=n-i;
+                }
+                else{
+                    right[k--]=stk1.top()-i;
+                }
+            }
+            stk1.push(i);
+        }
+        // cout<<endl;
+        // for(int i=0;i<n;i++)cout<<left[i]<<" ";
+        // cout<<endl;
+        // for(int i=0;i<n;i++)cout<<right[i]<<" ";
+
+        for(int i=0;i<n;i++)
+        {
+            maxArea=max(maxArea,heights[i]*left[i]+heights[i]*(right[i]-1));
+        }
+
+        return maxArea;
+    }
+};
+
+// Brute force
 
 class Solution {
 public:
