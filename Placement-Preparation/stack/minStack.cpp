2469eb63df1f90583cb8c92587a308cf98acd2a2
Prateek <prateekmohanty63@gmail.com>
2023-06-12 11:46:50 +0530

    Feat:Stack added minStack

--- Placement-Preparation/stack/minStack.cpp
+++ Placement-Preparation/stack/minStack.cpp
@@ -6,6 +6,55 @@ using namespace std;
 class MinStack {
 public:
     stack<int>stk;
+    stack<int>minStack;
+
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        stk.push(val);
+
+        if(minStack.empty())minStack.push(val);
+
+        else{
+            int topEle=minStack.top();
+            if(val<=topEle){
+                minStack.push(val);
+            }
+        }
+    }
+    
+    void pop() {
+        int ele=stk.top();
+        stk.pop();
+
+        if(minStack.top()==ele){
+            minStack.pop();
+        }
+    }
+    
+    int top() {
+        return stk.top();
+    }
+    
+    int getMin() {
+       return minStack.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+class MinStack {
+public:
+    stack<int>stk;
 
     MinStack() {
         
