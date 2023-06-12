f5b3418f4d41bb6e5b0036a06dbbaf7b627ba37e
Prateek <prateekmohanty63@gmail.com>
2023-06-12 12:41:26 +0530

    Feat:BST added binary search Tree Iterator

--- Placement-Preparation/BST/binarySearchTreeIterator.cpp
+++ Placement-Preparation/BST/binarySearchTreeIterator.cpp
@@ -1,6 +1,59 @@
 #include<iostream>
 #include<vector>
+#include<stack>
 using namespace std;
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// T(N)=O(n)
+// s(n)=O(h)
+class BSTIterator {
+public:
+    
+    stack<TreeNode *>stk;
+
+    void pushAll(TreeNode *root)
+    {
+        if(!root)return ;
+        stk.push(root);
+
+        pushAll(root->left);
+    }
+    BSTIterator(TreeNode* root) {
+        pushAll(root);
+    }
+    
+    int next() {
+        TreeNode *top=stk.top();
+        stk.pop();
+        if(top->right)pushAll(top->right);
+
+        return top->val;
+    }
+    
+    bool hasNext() {
+        
+        return !stk.empty();
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
 
 
 // Definition for a binary tree node.
@@ -13,7 +66,9 @@ using namespace std;
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
   };
  
- 
+
+ // space complexity: O(n)
+ // time complexity: O(n)
 class BSTIterator {
 public:
     vector<int>vec;
