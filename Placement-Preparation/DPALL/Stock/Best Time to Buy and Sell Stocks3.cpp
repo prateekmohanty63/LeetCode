35b4ab364087c70040ddf3d86a608c32bb92c37e
Prateek <prateekmohanty63@gmail.com>
2023-06-15 13:29:04 +0530

    Feat:DP added best-time-to-buy-sell-stocks3

--- Placement-Preparation/DPALL/Stock/Best Time to Buy and Sell Stocks3.cpp
+++ Placement-Preparation/DPALL/Stock/Best Time to Buy and Sell Stocks3.cpp
@@ -2,6 +2,48 @@
 #include<vector>
 using namespace std;
 
+
+// TLE 
+// T(N)=O(N^2)
+class Solution {
+public:
+    
+    int maxSell(vector<int>&prices,int i,int transLeft,vector<vector<int>>&dp)
+    {
+        // base case
+        int n=prices.size();
+
+        if(i>=n)return 0;
+
+        if(transLeft==0)return 0;
+
+        if(dp[i][transLeft]!=-1)return dp[i][transLeft];
+
+        int maxProfit=0;
+
+        int buyPrice=prices[i];
+
+        for(int j=i+1;j<n;j++){
+            int sellPrice=prices[j];
+
+            maxProfit=max(maxProfit,sellPrice-buyPrice+maxSell(prices,j+1,transLeft-1,dp));
+        }
+        maxProfit=max(maxProfit,maxSell(prices,i+1,transLeft,dp));
+        return dp[i][transLeft]=maxProfit;
+    }
+
+    int maxProfit(vector<int>& prices) {
+        
+        int n=prices.size();
+
+        vector<vector<int>>dp(n+1,vector<int>(3,-1));
+
+        int res=maxSell(prices,0,2,dp);
+
+        return res;
+    }
+};
+
 class Solution {
 public:
     
