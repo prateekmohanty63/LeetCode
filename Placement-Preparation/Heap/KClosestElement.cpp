9acbf2450966bc4b874a12fef9107572bf624b2e
Prateek <prateekmohanty63@gmail.com>
2023-06-17 10:48:54 +0530

    Feat:Heap added kclosest element

---
+++ Placement-Preparation/Heap/KClosestElement.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        
+        vector<int>res;
+
+        int n=arr.size();
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pr;
+
+        // pushing all the elements along with difference in the heap
+
+        for(int i=0;i<n;i++)
+        {
+            pair<int,int>p;
+            int diff=abs(x-arr[i]);
+            int ele=arr[i];
+            p.first=diff;
+            p.second=ele;
+
+            pr.push(p);
+        }
+
+        while(!pr.empty() && k!=0)
+        {
+            pair<int,int>fr=pr.top();
+            res.push_back(fr.second);
+            pr.pop();
+            k--;
+        }
+        sort(res.begin(),res.end());
+
+        return res;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
