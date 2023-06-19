6be3474aa98b761d6c63c637c9eb20cf98636879
Prateek <prateekmohanty63@gmail.com>
2023-06-19 12:46:45 +0530

    Feat:Sliding-window added frequency of most frequent element

--- Placement-Preparation/slidingWindow/frequencyOfMostFrequentElement.cpp
+++ Placement-Preparation/slidingWindow/frequencyOfMostFrequentElement.cpp
@@ -3,6 +3,52 @@
 #include<algorithm>
 using namespace std;
 
+// T(N)=O(n)
+// S(N)=O(1)
+
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        
+        sort(nums.begin(),nums.end());
+
+        int res=1;
+
+        int start=0;
+        int end=0;
+        int n=nums.size();
+        long long total=0;
+
+        while(end<n)
+        {
+           int winLength=end-start+1;
+           total+=nums[end];
+           long long sumReq=(long long)((long long)nums[end]*(long long)winLength)-k;
+
+           if(sumReq<=total)
+           {
+               res=max(res,winLength);
+           }
+           else{
+               int len=end-start+1;
+               long long currReq=(long long)((long long)nums[end]*(long long)len)-k;
+            // nums[end]*len>total+k
+
+               while(start<=end && currReq>total){
+                   
+                   total-=nums[start];
+                   start++;
+                   len--;
+                   currReq=(long long)((long long)nums[end]*(long long)len)-k;
+               }
+               res=max(res,end-start+1);
+           }
+            end++;
+        }
+        return res;
+    }
+};
+
 
 // T(N)=O(N^2)
 // S(N)=O(1)
