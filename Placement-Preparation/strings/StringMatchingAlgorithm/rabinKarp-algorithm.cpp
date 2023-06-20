a4a45f0f8ed70649df52b091b3230edd993e81e0
Prateek <prateekmohanty63@gmail.com>
2023-06-20 15:01:19 +0530

    Feat:String-algorithm  added rabin-karp algorithm

--- Placement-Preparation/strings/StringMatchingAlgorithm/rabinKarp-algorithm.cpp
+++ Placement-Preparation/strings/StringMatchingAlgorithm/rabinKarp-algorithm.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<cmath>
 #include <unordered_map>
 
 using namespace std;
@@ -10,12 +11,15 @@ using namespace std;
 // hence we can see that the average case time complexity of rabin karp is good
 // but the worst case complexity is O(n*m)
 
+// if we keep a normal hash functions , there would be lot of spurious hits
+
+// hence let the hash function be value(digit)*10^m-i
 
 int main()
 {
     unordered_map<char, int> mp;
 
-    string a = "aaabaab";
+    string a = "aaaab";
     string b = "aab";
 
     int n = a.length();
@@ -28,20 +32,21 @@ int main()
     }
 
     int target = 0;
-    for (auto it : b)
-        target += mp[it];
+    for (int i=0;i<m;i++)
+        target += mp[b[i]]*pow(10,m-1-i);
 
     int currCount = 0;
     int k=0;
+    int r=0;
 
     cout<<"target: "<<" "<<target<<endl;
 
     for (int i = 0; i < m; i++)
-        currCount += mp[a[i]];
+        currCount += (mp[a[i]]*pow(10,m-1-i));
 
     for (int i = 0; i <=n-m; i++)
     {
-        //cout<<currCount<<" ";
+       // cout<<currCount<<" ";
         if (target == currCount)
         {
             string currString = a.substr(i, m);
@@ -51,10 +56,17 @@ int main()
                 cout << currString<<" "<<"Match found at index: "
                      << " " << i << endl;
         }
-        currCount-=mp[a[k++]];
+      //  int pw=r-()
 
-        if(i+m<n)
-        currCount+=mp[a[i+m]];
+        // removing the first element from the sliding window sum 
+        currCount-=(mp[a[k++]]*pow(10,m-1));
+
+        // multiplying the rest of the sum by 10 , so that it moves left
+        currCount*=10;
+
+        // adding the next term to the window
+        currCount+=(mp[a[i+m]]*pow(10,0));
+        r++;
     }
 
     // for(auto it:mp)cout<<it.first<<" "<<it.second<<endl;
