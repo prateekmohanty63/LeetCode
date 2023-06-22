ddff072fb88b4e069fdcc4d230e254d911d37b90
Prateek <prateekmohanty63@gmail.com>
2023-06-22 13:08:36 +0530

    Feat:DFS added lowest common ancestor of binary tree

---
+++ Placement-Preparation/DFS/LowestCommonAncestorOfBinaryTree.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+ // Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+ 
+
+class Solution {
+public:
+    vector<vector<TreeNode*>>res;
+
+    void dfs(TreeNode *root,vector<TreeNode*>&vec)
+    {
+          // base case
+          if(!root)return ;
+          vec.push_back(root);
+          dfs(root->left,vec);
+          dfs(root->right,vec);
+    }
+    
+    void dfsNode(TreeNode *root)
+    {
+        if(!root)return ;
+
+        vector<TreeNode*>vec;
+        dfs(root,vec);
+        res.push_back(vec);
+
+        dfsNode(root->left);
+        dfsNode(root->right);
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        dfsNode(root);
+
+        for(int i=res.size()-1;i>=0;i--)
+        {
+            int count=0;
+            for(int j=0;j<res[i].size();j++)
+            {
+               if(res[i][j]==p)count++;
+               if(res[i][j]==q)count++;
+            }
+            if(count==2)return res[i][0];
+            // cout<<endl;
+        }
+
+        return NULL;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
