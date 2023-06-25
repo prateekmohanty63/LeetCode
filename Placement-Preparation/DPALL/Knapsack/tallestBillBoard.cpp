689fbaa84bc114375f17d077e012ddd3ffbce995
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-06-25 21:01:42 +0530

    Feat:DP-KNAPSACK Added tallest billboard

--- Placement-Preparation/DPALL/Knapsack/tallestBillBoard.cpp
+++ Placement-Preparation/DPALL/Knapsack/tallestBillBoard.cpp
@@ -4,6 +4,43 @@
 #include<algorithm>
 using namespace std;
 
+// T(N)=O(n*(5000))
+// S(N)=O(N)
+
+class Solution {
+public:
+    int solve(vector<int>&rods,int index,int diff,vector<vector<int>>&dp)
+    {
+        // base case
+        int n=rods.size();
+
+        if(index>=n)
+        {
+            if(diff==0)return 0;
+
+            else return INT_MIN;
+        }
+         // we are adding 5000 ,because difference can become -5000 at max
+        if(dp[index][diff+5000]!=-1)return dp[index][diff+5000];
+
+        int addRod1=rods[index]+solve(rods,index+1,diff+rods[index],dp);
+        int addRod2=rods[index]+solve(rods,index+1,diff-rods[index],dp);
+        int nothing=solve(rods,index+1,diff,dp);
+
+        return dp[index][diff+5000]=max({addRod1,addRod2,nothing});
+    }
+    int tallestBillboard(vector<int>& rods) {
+        
+        int n=rods.size();
+        vector<vector<int>>dp(n,vector<int>(10001,-1));
+
+        int res=solve(rods,0,0,dp)/2;
+
+        return res;
+    }
+};
+
+
 // This solution will also give TLE
 
 // and it we take a 3d DP of dp[n][5001][5001]  (max sum can be 5000 , given in constraints)
