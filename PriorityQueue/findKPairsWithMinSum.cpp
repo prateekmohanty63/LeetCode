8b1d9092345991667168893398a5e991b99d297f
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-06-27 12:52:49 +0530

    Feat:Priority-queue added find k pairs with min sum

--- PriorityQueue/findKPairsWithMinSum.cpp
+++ PriorityQueue/findKPairsWithMinSum.cpp
@@ -4,8 +4,49 @@
 #include<algorithm>
 using namespace std;
 
+// T(N)=O(Klogk)
+// S(N)=O(K)
 
-// T(N)=O(N^2)
+class Solution {
+public:
+ 
+   
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        // NOTE in priority queue comparator operator is always written opposite to what is required
+        
+         auto compare=[&](const vector<int>&a,const vector<int>&b){
+           return a[0]+a[1]>b[0]+b[1];
+       };
+        vector<vector<int>>res;
+
+       priority_queue<vector<int>,vector<vector<int>>,decltype(compare)>pq(compare);
+
+       
+       for(int i=0;i<nums1.size() && i<k;i++)
+       {
+           pq.push({nums1[i],nums2[0],0});
+       }
+
+       while(k>0 && !pq.empty())     // O(K)
+      {
+          vector<int>vec=pq.top();
+          int currIndex=vec[2];
+          pq.pop();
+          res.push_back({vec[0],vec[1]});     // O(log(k))
+          k--;
+
+          if(currIndex==nums2.size()-1)continue;
+
+          pq.push({vec[0],nums2[currIndex+1],currIndex+1});
+          
+
+
+      }      
+      return res;
+    }
+};
+
+// T(N)=O(n*m*log(k))
 // S(N)=O(K)
 // TLE
 
@@ -20,12 +61,12 @@ public:
         int n=nums1.size();
         int m=nums2.size();
 
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
+        for(int i=0;i<n;i++)    // O(N)
+        { 
+            for(int j=0;j<m;j++)   // O(M)
             {
 
-                q.push({nums1[i]+nums2[j],{nums1[i],nums2[j]}});
+                q.push({nums1[i]+nums2[j],{nums1[i],nums2[j]}});   // log(k)
 
                 if(q.size()>k)q.pop();
             }
