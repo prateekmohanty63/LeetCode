bb328c41c6fb900f01fa666e21b0de995ea07c53
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-06-28 15:49:38 +0530

    Feat:DP added number of music playlist

--- Placement-Preparation/DPALL/MultiDimensionalDP/numberOfMusicPlaylist.cpp
+++ Placement-Preparation/DPALL/MultiDimensionalDP/numberOfMusicPlaylist.cpp
@@ -1,8 +1,68 @@
 #include<iostream>
+#include<vector>
 #include<unordered_map>
 
 using namespace std;
 
+// T(N)=O(n*goal*k)
+
+
+class Solution {
+public:
+int mod=1000000007;
+int nVal;
+int kVal;
+    long long solve(int n,int goal,int k,vector<vector<vector<long long>>>&dp)
+    {
+        if(goal==0)
+        {
+            // if all number have not been taken atleast once
+            if(n<=0)return 1;
+
+            return 0;
+        }
+
+        if(dp[n][goal][k]!=-1)return dp[n][goal][k];
+
+        long long ans=0;
+
+        if(k>0)
+        {
+            // I have to take unique songs
+            ans+=n*solve(n-1,goal-1,k-1,dp)%mod;
+        }
+        else{
+            // take unique song or previously played songs
+           if(n<=0)
+           {
+               // taking repeated value 
+              ans+=(nVal-kVal)*solve(n,goal-1,k,dp)%mod;
+           }
+           else{
+               // taking repeated value
+             ans+=(nVal-kVal-n)*solve(n,goal-1,k,dp)%mod;
+           }
+           if(n>0){
+               // taking unique value
+             ans+=n*solve(n-1,goal-1,k,dp)%mod;
+           }
+        }
+        return dp[n][goal][k]=ans%mod;
+    }
+    int numMusicPlaylists(int n, int goal, int k) {
+        
+        nVal=n;
+        kVal=k;
+       vector<vector<vector<long long>>> dp(n+1, vector<vector<long long>>(goal+1, vector<long long>(k+2, -1)));
+        int res=solve(n,goal,k+1,dp);
+       // for(int i=0;i<k;i++)res*=(i+1);
+
+        return res;
+
+    }
+};
+
+// wrong logic
 class Solution {
 public:
     int mod=1000000007;
