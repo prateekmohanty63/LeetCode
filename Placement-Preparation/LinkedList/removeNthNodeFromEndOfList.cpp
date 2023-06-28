1394adf47d9dd60a46695f10040567dd008f4d64
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-06-28 12:01:18 +0530

    Feat:Linked list added remove nth node from end of list

---
+++ Placement-Preparation/LinkedList/removeNthNodeFromEndOfList.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+
+ // Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+ 
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        if(!head)return NULL;
+        if(head && !head->next)return NULL;
+
+        ListNode* dummy=new ListNode(-1);
+        dummy->next=head;
+        ListNode* fast=dummy;
+        ListNode* slow=dummy;
+
+        while(fast!=NULL && n!=0)
+        {
+            fast=fast->next;
+            n--;
+        }
+
+        while(fast->next!=NULL)
+        {
+            fast=fast->next;
+            slow=slow->next;
+        }
+        slow->next=slow->next->next;
+
+        return dummy->next;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
