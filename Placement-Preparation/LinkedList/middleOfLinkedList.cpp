3c6b6cd20b915eb446c07402c94c5f224c5e6f7c
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-06-28 10:34:37 +0530

    Feat:Linked list added middle of linked list

---
+++ Placement-Preparation/LinkedList/middleOfLinkedList.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+
+ // Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        
+        ListNode *slow=head;
+        ListNode *fast=head;
+
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+
+        return slow;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
