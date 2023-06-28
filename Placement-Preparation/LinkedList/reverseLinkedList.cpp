ca5383fab77c3692eab6a84869b62c21f3bc1223
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-06-28 10:22:36 +0530

    Feat:linked-list added reverse linked list

--- Placement-Preparation/LinkedList/reverseLinkedList.cpp
+++ Placement-Preparation/LinkedList/reverseLinkedList.cpp
@@ -3,6 +3,43 @@
 using namespace std;
 
 
+// T(N)=O(N)
+// S(N)=O(1)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        if(head==NULL)return NULL;
+
+        if(head->next==NULL)return head;
+
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* nxt=head->next;
+
+        while(nxt!=NULL)
+        {
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+            nxt=nxt->next;
+        }
+        curr->next=prev;
+        return curr;
+    }
+};
+
+
  // Definition for singly-linked list.
   struct ListNode {
       int val;
