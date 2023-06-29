a53d6d0fac98c0461ac531f5dfe28e730f54ca75
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-06-29 13:00:57 +0530

    Feat:Linked list added palindome linked list

--- Placement-Preparation/LinkedList/palindromeLinkedList.cpp
+++ Placement-Preparation/LinkedList/palindromeLinkedList.cpp
@@ -11,7 +11,77 @@ using namespace std;
       ListNode(int x) : val(x), next(nullptr) {}
       ListNode(int x, ListNode *next) : val(x), next(next) {}
   };
+
+
+// T(N)=O(N)
+// S(N)=O(1)
+
+class Solution {
+public:
+    ListNode* reverseLinkedList(ListNode* head)
+    {
+        if(head==NULL)return NULL;
+        if(head && head->next==NULL)return head;
+
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* nxt=head->next;
+
+        while(nxt!=NULL)
+        {
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+            nxt=nxt->next;
+        }
+        curr->next=prev;
+
+        return curr;
+    }
+    bool isPalindrome(ListNode* head) {
+
+        if(head==NULL)return true;
+
+        if(head!=NULL && head->next==NULL)return true;
+        
+        ListNode* head1=head;
+        ListNode* slow=head;
+        ListNode* fast=head;
+       
+
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+
+        ListNode* head2=reverseLinkedList(slow);
+
+        ListNode* temp2=head2;
+        ListNode* temp1=head1;
+
+        // while(temp2!=NULL)
+        // {
+        //     cout<<temp2->val<<" ";
+        //     temp2=temp2->next;
+        // }
+
+        while(temp1!=NULL && temp2!=NULL)
+        {
+            if(temp1->val!=temp2->val)return false;
+            temp1=temp1->next;
+            temp2=temp2->next;
+        }
+
+        return true;
+    }
+};
+
+
  
+
+ // T(N)=O(N)
+ // S(N)=O(N)
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
