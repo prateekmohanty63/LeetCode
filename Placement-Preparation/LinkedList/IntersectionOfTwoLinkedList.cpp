af8a80730fbe143258036abe2f0360a7fe02491f
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-06-29 10:11:30 +0530

    Feat:Linkelist added intersection of 2 linked list

--- Placement-Preparation/LinkedList/IntersectionOfTwoLinkedList.cpp
+++ Placement-Preparation/LinkedList/IntersectionOfTwoLinkedList.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* temp1=headA;
+        ListNode* temp2=headB;
+
+       if(headA==NULL || headB==NULL)return NULL;
+
+       while(temp1!=temp2)
+       {
+           temp1=temp1==NULL? headB:temp1->next;
+           temp2=temp2==NULL ? headA:temp2->next;
+       }
+
+     return temp1;
+    }
+};
+
  // Definition for singly-linked list.
   struct ListNode {
       int val;
