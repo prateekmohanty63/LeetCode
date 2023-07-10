31613a03d1978d424753b01dcaf8049ab725c1ee
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-07-10 13:03:10 +0530

    Feat:BinaryTree added is balanced binary tree

--- Placement-Preparation/BinaryTree/balancedBinaryTree.cpp
+++ Placement-Preparation/BinaryTree/balancedBinaryTree.cpp
@@ -12,10 +12,38 @@ using namespace std;
       TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
   };
+
+// T(N)=O(N)
+// S(N)=O(H) = O(N) (skewed tree)
+  pair<bool,int> isBalancedFast(TreeNode* root)
+    {
+        // base case
+        if(!root)return {true,0};
+
+        pair<bool,int>left=isBalancedFast(root->left);
+        pair<bool,int>right=isBalancedFast(root->right);
+
+        bool leftBalanced=left.first;
+        bool rightBalanced=right.first;
+
+        bool diff=abs(left.second-right.second)<=1; 
+
+        pair<bool,int>ans;
+        ans.first=leftBalanced && rightBalanced && diff;
+        ans.second=max(left.second,right.second)+1;     // height = max(leftHeight,rightHeight)+1
+
+        return ans;
+    }
+    bool isBalanced(TreeNode* root) {
+         // base case
+         if(!root)return true;
+         return isBalancedFast(root).first;
+
+    }
  
  // T(N)=O(N^2)
  // S(N)=O(h)  
- 
+
 class Solution {
 public:
     int height(TreeNode* root)
