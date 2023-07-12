2d4c9511eb63545ea761ff59561ebcc1b2813d0c
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-07-12 13:54:17 +0530

    Feat:Binary tree added all nodes in distance k

--- Placement-Preparation/BinaryTree/allNodesDistanceKInBinaryTree.cpp
+++ Placement-Preparation/BinaryTree/allNodesDistanceKInBinaryTree.cpp
@@ -1,7 +1,89 @@
 #include<iostream>
 #include<vector>
 #include<unordered_map>
+#include<queue>
 using namespace std;
+
+
+
+ // Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+
+// T(N)=O(N)
+// S(N)=O(N)
+ 
+class Solution {
+public:
+   unordered_map<TreeNode*,TreeNode*>parentPointers;
+   unordered_map<TreeNode*,bool>vis;
+   vector<int>res;
+
+   void solve(TreeNode* root,unordered_map<TreeNode*,TreeNode*>&mp,int &k)
+   {   
+       queue<pair<TreeNode*,int>>q;
+       q.push({root,0});
+       vis[root]=true;
+       
+
+       while(!q.empty())
+       {
+           TreeNode* curr=q.front().first;
+           int currLevel=q.front().second;
+           cout<<curr->val<<" "<<currLevel<<endl;
+           q.pop();
+
+           if(currLevel==k)res.push_back(curr->val);
+
+           if(curr->left!=NULL && vis.find(curr->left)==vis.end())
+           {
+               vis[curr->left]=true;
+               q.push({curr->left,currLevel+1});
+           }
+           if(curr->right!=NULL && vis.find(curr->right)==vis.end())
+           {
+               vis[curr->right]=true;
+               q.push({curr->right,currLevel+1});
+           }
+           TreeNode* parent=mp[curr];
+           if(parent!=NULL && vis.find(parent)==vis.end())
+           {
+               vis[parent]=true;
+               q.push({parent,currLevel+1});
+           }
+       }
+   }
+
+   void inOrder(TreeNode *root)
+   {
+       // base case
+       if(!root)return ;
+
+       if(root->left){
+       parentPointers[root->left]=root;
+       inOrder(root->left);
+       }
+
+       if(root->right){
+       parentPointers[root->right]=root;
+       inOrder(root->right);
+       }
+   }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        
+        inOrder(root);
+
+       // for(auto it:parentPointers)cout<<it.first->val<<" "<<it.second->val<<endl;
+
+        solve(target,parentPointers,k);
+
+        return res;
+    }
+};
 
 
 // approach-1 (failing for some cases || logic wrong)
