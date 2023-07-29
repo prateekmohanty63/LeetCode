ff11573c1cbe2df9e4fce417ca65cc017f613137
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-07-29 15:55:15 +0530

    Feat:DP-Bit-manipulation added combination-sum-3

--- Placement-Preparation/DPALL/BitManipulation/combinationSum3.cpp
+++ Placement-Preparation/DPALL/BitManipulation/combinationSum3.cpp
@@ -1,8 +1,55 @@
 #include<iostream>
 #include<vector>
 #include<map>
+#include<algorithm>
 
 using namespace std;
+
+class Solution {
+public:
+   vector<vector<int>>res;
+   map<vector<int>,bool>mp;
+   
+   void solve(int k,int n,int mask,vector<int>&vec)
+   {
+       // base case
+       if(n==0 && k==0)
+       {
+          vector<int>v=vec;
+          sort(v.begin(),v.end());
+           if(mp.find(v)==mp.end())
+           {
+               res.push_back(v);
+               mp[v]=true;
+           }
+            return ;
+       }
+
+       if(n<=0 || k<=0)return ;
+
+       for(int i=1;i<=9;i++)
+       {
+           int isSet=(mask&(1<<i));
+
+           if(isSet==0)
+           {
+               int newMask=(mask|(1<<i));
+               vec.push_back(i);
+               solve(k-1,n-i,newMask,vec);
+               vec.pop_back();
+           }
+       }
+   }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        
+        if(n>=46)return {};
+        vector<int>vec;
+        solve(k,n,0,vec);
+
+        return res;
+    }
+};
 
 class Solution {
 public:
