3c5c6b114d19dfb1aa635862ebae440146673659
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-08-04 18:34:38 +0530

    Feat:BFS_DFS added jumpgame-7

--- Placement-Preparation/BFS_DFS/jumpGame7.cpp
+++ Placement-Preparation/BFS_DFS/jumpGame7.cpp
@@ -1,6 +1,40 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
+
+// T(N)= Beats 81.35%of users with C++
+// S(N)= Beats 50.35%of users with C++
+
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        
+        int n=s.length();
+
+        if(s[n-1]=='1')return false;
+
+        int i=0;
+        queue<int>q;
+        q.push(0);
+        int curr_max=0;
+
+        while(!q.empty())
+        {
+            i=q.front();
+            q.pop();
+
+            if(i==n-1)return true;
+
+            for(int j=max(i+minJump,curr_max);j<=min(i+maxJump,n-1);j++)
+            {
+                if(s[j]=='0')q.push(j);
+            }
+            curr_max=min(i+maxJump+1,n);
+        }
+        return false;
+    }
+};
 
 
 // TLE 
