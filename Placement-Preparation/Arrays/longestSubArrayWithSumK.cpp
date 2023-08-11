937626bf892dc4cc70088f39a529769d3541402f
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-08-11 12:00:54 +0530

    Feat:Array added longest subarray with sum k

--- Placement-Preparation/Arrays/longestSubArrayWithSumK.cpp
+++ Placement-Preparation/Arrays/longestSubArrayWithSumK.cpp
@@ -34,6 +34,74 @@ class Solution{
 
 };
 
+
+// T(n)=O(n)
+// S(n)=O(n)
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int lenOfLongSubarr(int A[],  int N, int K) 
+    { 
+        // Complete the function
+        
+        int ans=INT_MIN;
+       
+       
+        unordered_map<long long,int>mp;
+        
+       long long sum=0;
+        
+        for(int i=0;i<N;i++)
+        {
+            sum+=A[i];
+            
+            if(sum==K)ans=max(ans,i+1);
+            
+            long long req=sum-K;
+            if(mp.find(req)!=mp.end()){
+            ans=max(ans,i-mp[req]);
+            }
+            
+            if(mp.find(sum)==mp.end())
+            mp[sum]=i;
+        }
+        
+        if(ans==INT_MIN)return 0;
+        return ans;
+    } 
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	//code
+	
+	int t;cin>>t;
+	while(t--)
+	{
+	    int n, k;
+	    cin>> n >> k;
+	    int a[n];
+	    
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	   Solution ob;
+	   cout << ob.lenOfLongSubarr(a, n , k)<< endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
+
 //{ Driver Code Starts.
 
 int main() {
