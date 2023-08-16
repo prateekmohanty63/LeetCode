cf8f660da23da45d81a08dc52ead688fdd5f4d06
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-08-16 22:46:36 +0530

    Feat:Hashmap added sort an array according to other

--- Placement-Preparation/HashMap/sortAnArrayAccordingToOther.cpp
+++ Placement-Preparation/HashMap/sortAnArrayAccordingToOther.cpp
@@ -63,39 +63,7 @@ class Solution{
 
 int main(int argc, char *argv[]) 
 { 
-	
-	int t;
-	
-	cin >> t;
-	
-	while(t--){
-	    
-	    int n, m;
-	    cin >> n >> m;
-	    
-	    vector<int> a1(n);
-	    vector<int> a2(m);
-	    
-	    for(int i = 0;i<n;i++){
-	        cin >> a1[i];
-	    }
-	    
-	    for(int i = 0;i<m;i++){
-	        cin >> a2[i];
-	    }
-	    
-	    Solution ob;
-	    a1 = ob.sortA1ByA2(a1, n, a2, m); 
-	
-	   
-	    for (int i = 0; i < n; i++) 
-		    cout<<a1[i]<<" ";
-		
-	    cout << endl;
-	    
-	    
-	}
-	return 0; 
+	 
 } 
 
 // } Driver Code Ends
